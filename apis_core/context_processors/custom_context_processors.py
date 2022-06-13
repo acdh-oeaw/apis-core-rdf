03f0e4bb6984cd5185838af8dec26b40bd7cdf52
KK <dev-kk@oeaw.ac.at>
2022-06-13 11:26:15 +0200

    return more sensible values for templates

--- apis_core/context_processors/custom_context_processors.py
+++ apis_core/context_processors/custom_context_processors.py
@@ -14,10 +14,14 @@ def add_entities(request):
     from apis_core.apis_entities.models import AbstractEntity
 
     entities_classes = AbstractEntity.get_all_entity_classes() or []
-    entities_uris = [e.__name__.lower() for e in entities_classes]
+    # create (uri, label) tuples for entities for use in templates
+    entities_links = [(e.__name__.lower(), e._meta.verbose_name_plural.title()) for e in entities_classes]
+    entities_uris = [u[0] for u in entities_links]  # TODO kk for compatibility, see below
 
     res = {
-        'entities_list': entities_uris,
+        'entities_list': entities_uris,  # TODO kk keep for compatibility; remove when not used in templates anymore
+        'entities_classes': entities_classes,
+        'entities_links': entities_links,
         'request': request
     }
     return res
