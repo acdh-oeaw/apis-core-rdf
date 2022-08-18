12b1759554f703746bdd85983f861d76f97441b0
steffres <stefan.resch@oeaw.ac.at>
2022-08-18 17:40:15 +0200

    Refactoring function sort_fields_list, inserted new function

--- apis_core/apis_entities/forms.py
+++ apis_core/apis_entities/forms.py
@@ -150,12 +150,35 @@ def get_entities_form(entity):
                     # append to unsorted list, so that it can be sorted and afterwards attached to accordion group acc_grp1
                     fields_list_unsorted.append(f)
 
-            def sort_fields_list(list_unsorted, entity_label):
-                pass
+            def sort_fields_list(elem_unordered_list, entity_label):
+                """
+                Sorts a list of model fields according to defined order in entity settings.
+                If no settings exists, the unordered list is returned.
+                """
+
+                elem_ordered_list = settings.APIS_ENTITIES[entity_label].get("form_order")
+                if elem_ordered_list is not None:
+                    elem_final_list = []
+
+                    # quick check if settings correlate with model
+                    for elem_ordered in elem_ordered_list:
+                        if elem_ordered not in elem_unordered_list:
+                            raise Exception(f"Found field {elem_ordered} in form_order setting that does not exist on the model")
+
+                    # use the defined order list
+                    elem_final_list.extend(elem_ordered_list)
+
+                    # append the unordered list to the final list (and check if an element hasn't already been added)
+                    for elem_unordered in elem_unordered_list:
+                        if elem_unordered not in elem_ordered_list:
+                            elem_final_list.append(elem_unordered)
+
+                    return elem_final_list
+                else:
+                    return elem_unordered_list
 
             # sort field list, iterate over it and append each element to the accordion group
-            # for f in sort_fields_list(fields_list_unsorted, entity):
-            for f in fields_list_unsorted:
+            for f in sort_fields_list(fields_list_unsorted, entity):
                 acc_grp1.append(f)
 
             self.helper.layout = Layout(Accordion(acc_grp1, acc_grp2))
