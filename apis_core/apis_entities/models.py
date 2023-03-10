acdh-oeaw/apis-core-rdf

commit eb0d4bc80635d55e9ba7aa000ff614a0759c0f3a

running away from past responsibilites, hehe. But in serious: just rename TODOs from sresch to general

apis_core/apis_entities/models.py
@@ -309,11 +309,7 @@ class AbstractEntity(RootObject):
         place_instance.get_related_relation_classes()
         -> [ InstitutionPlace, PersonPlace, PlaceEvent, PlacePlace, PlaceWork ]
         """
-
-        # TODO __sresch__ : check for best practice on local imports vs
-        #  circularity problems.
         from apis_core.apis_relations.models import AbstractRelation
-
         return AbstractRelation.get_relation_classes_of_entity_class(cls)
 
     @classmethod
@@ -329,10 +325,7 @@ class AbstractEntity(RootObject):
         -> ["institutionplace_set", "personplace_set", "placeevent_set",
         "placeplace_set", "placework_set"]
         """
-        # TODO __sresch__ : check for best practice on local imports vs
-        #  circularity problems.
         from apis_core.apis_relations.models import AbstractRelation
-
         return AbstractRelation.get_relation_field_names_of_entity_class(cls)
 
     def get_related_relation_instances(self):
@@ -384,7 +377,7 @@ class AbstractEntity(RootObject):
             relationtype_classes = []
             relationtype_names = []
 
-            # TODO __sresch__: check for best practice on local imports vs.
+            # TODO: check for best practice on local imports vs.
             #  circularity problems.
             from apis_core.apis_vocabularies.models import AbstractRelationType
 

apis_core/apis_labels/models.py
@@ -45,7 +45,7 @@ class Label(models.Model):
         verbose_name="End",
     )
 
-    # TODO __sresch__ add related_name="label_set" here to be consistent with other usages throughout django
+    # TODO  add related_name="label_set" here to be consistent with other usages throughout django
     temp_entity = models.ForeignKey(
         "apis_entities.TempEntityClass", on_delete=models.CASCADE
     )

apis_core/apis_metainfo/models.py
@@ -590,7 +590,7 @@ class Text(models.Model):
         super().save(*args, **kwargs)
 
 
-# TODO __sresch__ : Move this somewhere else so that it can be imported at several places (right now it's redundant with copies)
+# TODO: Move this somewhere else so that it can be imported at several places (right now it's redundant with copies)
 from django.db.models.fields.related_descriptors import ForwardManyToOneDescriptor
 
 

apis_core/apis_relations/models.py
@@ -121,7 +121,7 @@ class Property(RootObject):
         return self
 
 
-# TODO __sresch__ : comment and explain
+# TODO: comment and explain
 def subj_or_obj_class_changed(sender, is_subj, **kwargs):
     def cascade_subj_obj_class_to_children(
         contenttype_to_add_or_remove,
@@ -135,7 +135,7 @@ def subj_or_obj_class_changed(sender, is_subj, **kwargs):
 
             for class_parent in class_current.__bases__:
 
-                # TODO __sresch__ : Avoid ContentType DB fetch
+                # TODO: Avoid ContentType DB fetch
                 contenttype_parent = ContentType.objects.filter(
                     model=class_parent.__name__
                 )
@@ -153,7 +153,7 @@ def subj_or_obj_class_changed(sender, is_subj, **kwargs):
             class_current = contenttype_current.model_class()
 
             for class_child in class_current.__subclasses__():
-                # TODO __sresch__ : Avoid ContentType DB fetch
+                # TODO: Avoid ContentType DB fetch
                 contenttype_child = ContentType.objects.get(model=class_child.__name__)
                 child_list.append(contenttype_child)
                 child_list.extend(get_all_children(contenttype_child))
@@ -587,7 +587,7 @@ class RelationPublishedQueryset(models.QuerySet):
 #         return None
 
 
-# TODO __sresch__ : Move this somewhere else so that it can be imported at several places (right now it's redundant with copies)
+# TODO: Move this somewhere else so that it can be imported at several places (right now it's redundant with copies)
 from django.db.models.fields.related_descriptors import ForwardManyToOneDescriptor
 
 

apis_core/apis_entities/filters.py
@@ -26,9 +26,6 @@ from collections import OrderedDict
 # where then such a key-value from the settings is overriding the respective key-value of a filter defined in this module
 # (e.g. using a different method)
 
-# TODO __sresch__ : Turn the logic of returing a filter object into a singleton pattern to avoid redundant instantiations
-# TODO __sresch__ : use the order of list of filter fields in settings
-
 
 #######################################################################
 #
@@ -36,7 +33,6 @@ from collections import OrderedDict
 #
 #######################################################################
 
-# TODO __sresch__ : Do this better
 from apis_core.helper_functions import caching
 
 fields_to_exclude = getattr(settings, "APIS_RELATIONS_FILTER_EXCLUDE", [])
@@ -66,7 +62,7 @@ class GenericEntityListFilter(django_filters.FilterSet):
         queryset=Collection.objects.all()
     )
 
-    # TODO __sresch__ : look into how the date values can be intercepted so that they can be parsed with the same logic as in edit forms
+    # TODO: look into how the date values can be intercepted so that they can be parsed with the same logic as in edit forms
     start_date = django_filters.DateFromToRangeFilter()
     end_date = django_filters.DateFromToRangeFilter()
 
@@ -232,7 +228,7 @@ class GenericEntityListFilter(django_filters.FilterSet):
             return "__icontains", value
 
     def name_label_filter(self, queryset, name, value):
-        # TODO __sresch__ : include alternative names queries
+        # TODO: include alternative names queries
         lookup, value = self.construct_lookup(value)
 
         queryset_related_label = queryset.filter(**{"label__label" + lookup: value})
@@ -444,7 +440,7 @@ class GenericEntityListFilter(django_filters.FilterSet):
 #     name = django_filters.CharFilter(method="person_name_filter", label="Name or Label of person")
 #
 #
-#     # TODO __sresch__ : look into how the meta class can be inherited from the superclass so that the Meta class' exclude attribute must not be defined multiple times
+#     # TODO: look into how the meta class can be inherited from the superclass so that the Meta class' exclude attribute must not be defined multiple times
 #     class Meta:
 #         model = Person
 #         # exclude all hardcoded fields or nothing, however this exclude is only defined here as a temporary measure in
@@ -468,7 +464,7 @@ class GenericEntityListFilter(django_filters.FilterSet):
 #
 # class PlaceListFilter(GenericListFilter):
 #
-#     # TODO __sresch__ : decide on margin tolerance of input, for now the number must be precise
+#     # TODO: decide on margin tolerance of input, for now the number must be precise
 #     lng = django_filters.NumberFilter(label='Longitude')
 #     lat = django_filters.NumberFilter(label='Latitude')
 #

apis_core/apis_entities/urls.py
@@ -81,7 +81,7 @@ urlpatterns = [
         GenericNetworkEntitiesAutocomplete.as_view(),
         name="generic_network_entities_autocomplete",
     ),
-    # TODO __sresch__ : This seems unused. Remove it once sure
+    # TODO: This seems unused. Remove it once sure
     # url(r"^detail/work/(?P<pk>[0-9]+)$",
     #     detail_generic.WorkDetailView.as_view(), name="work_detail"),
     path("place/geojson/", views.getGeoJson, name="getGeoJson"),

apis_core/apis_labels/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from .models import Label
 
 
-# TODO __sresch__ : inspect if this should be removed or adapted
+# TODO: inspect if this should be removed or adapted
 # since this class seems redundant as the same functionality is provided in apis_relations/forms.py in class EntityLabelForm there
 #
 # This class LabelForm here is only refered to in apis_labels/views.py which would be loaded by the respective urls in apis_labels/urls.py

apis_core/apis_metainfo/validators.py
@@ -4,7 +4,7 @@ import re
 
 from django.core.validators import RegexValidator
 
-# TODO __sresch__ : remove once sure
+# TODO: remove once sure
 date_validator = RegexValidator(
     regex=re.compile(r"^([0-9]{1,2}\.[0-9]{1,2}\.)?[0-9]{4}$"),
     message="Please enter a valide date: (DD).(MM).YYYY",

commit 870d0bbb0bdc6fdbc38b73463e4ee366ea56a34c

Fix formatting issues introduced in eb0d4bc and 10548ec

apis_core/apis_entities/models.py
@@ -310,6 +310,7 @@ class AbstractEntity(RootObject):
         -> [ InstitutionPlace, PersonPlace, PlaceEvent, PlacePlace, PlaceWork ]
         """
         from apis_core.apis_relations.models import AbstractRelation
+
         return AbstractRelation.get_relation_classes_of_entity_class(cls)
 
     @classmethod
@@ -326,6 +327,7 @@ class AbstractEntity(RootObject):
         "placeplace_set", "placework_set"]
         """
         from apis_core.apis_relations.models import AbstractRelation
+
         return AbstractRelation.get_relation_field_names_of_entity_class(cls)
 
     def get_related_relation_instances(self):

apis_core/apis_vocabularies/models.py
@@ -197,12 +197,14 @@ class LabelType(VocabsBaseClass):
 
     pass
 
+
 # TODO RDF: Remove this
 @reversion.register(follow=["vocabsbaseclass_ptr"])
 class CollectionType(VocabsBaseClass):
     """e.g. reseachCollection, importCollection"""
 
     pass
+
 
 # TODO RDF: Remove this
 @reversion.register(follow=["vocabsbaseclass_ptr"])