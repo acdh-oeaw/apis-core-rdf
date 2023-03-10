10b6cace79594b573e59f57879f7af2e6f8410ea
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-03-10 11:59:12 +0100

    Reformat code in apis with black

--- apis/settings/settings_test_ci.py
+++ apis/settings/settings_test_ci.py
@@ -1,12 +1,12 @@
 from .base import *
 
-INSTALLED_APPS.remove('webpage')
-INSTALLED_APPS.remove('charts')
-INSTALLED_APPS.remove('apis_ontology')
+INSTALLED_APPS.remove("webpage")
+INSTALLED_APPS.remove("charts")
+INSTALLED_APPS.remove("apis_ontology")
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': '/tmp/db.sqlite3',
-  },
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "/tmp/db.sqlite3",
+    },
 }
