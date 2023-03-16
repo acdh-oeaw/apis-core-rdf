97db0ef92a12697f812ddb6e6e43431f6872cffe
steffres <stefan.resch@oeaw.ac.at>
2023-03-16 09:59:34 +0100

    fix wrong global string replacement

--- apis_core/apis_metainfo/migrations/0002_auto_20220201_1241.py
+++ apis_core/apis_metainfo/migrations/0002_auto_20220201_1241.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='rootobject',
-            name='self_contenttype',
+            name='self_content_type',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype'),
         ),
         migrations.AddField(
