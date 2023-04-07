b9f4ad2de6be2509a2b4d46b2227e9a6f87a5fc6
Birger Schacht <birger@rantanplan.org>
2023-04-07 10:55:38 +0200

    fix: Set the default_auto_field in all the apps
    Maintaining the historical behavior, the default value for
    DEFAULT_AUTO_FIELD is AutoField. We are setting this value on a per app
    basis to avoid unwanted migrations for users.
    Starting with 3.2 new apps are generated with
    AppConfig.default_auto_field set to BigAutoField.
    See https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys

--- apis_core/apis_relations/apps.py
+++ apis_core/apis_relations/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class RelationsConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
     name = "apis_core.apis_relations"
--- apis_core/apis_vocabularies/apps.py
+++ apis_core/apis_vocabularies/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class VocabulariesConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
     name = "apis_core.apis_vocabularies"
