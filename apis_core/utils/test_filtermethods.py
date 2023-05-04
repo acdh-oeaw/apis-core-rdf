42ddec02ff15aed077942ffaa463b825b4c5e224
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-05-04 10:56:28 +0200

    tests: add tests for `utils.filtermethods.construct_lookup`

---
+++ apis_core/utils/test_filtermethods.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2023 Birger Schacht
+# SPDX-License-Identifier: MIT
+
+from django.test import TestCase
+
+from .filtermethods import construct_lookup
+
+lookups = {
+    "*foo*": ("__icontains", "foo"),
+    "foo": ("__icontains", "foo"),
+    "*foo": ("__iendswith", "foo"),
+    "foo*": ("__istartswith", "foo"),
+    '"foo"': ("__iexact", "foo"),
+}
+
+
+class FilterMethodstest(TestCase):
+    def test_lookup(self):
+        for lookup in lookups:
+            self.assertEqual(construct_lookup(lookup), lookups[lookup])
