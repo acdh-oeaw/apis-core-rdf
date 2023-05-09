f64c90ec0776c0c165f482e626f53d42683b9299
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-05-09 14:50:18 +0200

    feat(apis_entities): introduce apis_entities.mixins
    This commit introduces the EntityMixin and the EntityInstanceMixin which
    can be used in views that handle entities. This way all those views have
    access to the same class attributes and it reduces LOC.

--- apis_core/apis_entities/edit_generic.py
+++ apis_core/apis_entities/edit_generic.py
@@ -28,24 +28,23 @@ from .views import get_highlighted_texts
 from .views import set_session_variables
 from ..apis_vocabularies.models import TextType
 from apis_core.utils import caching
+from apis_core.apis_entities.mixins import EntityMixin, EntityInstanceMixin
 
 if "apis_highlighter" in settings.INSTALLED_APPS:
     from apis_highlighter.forms import SelectAnnotatorAgreement
 
 
 @method_decorator(login_required, name="dispatch")
-class GenericEntitiesEditView(View):
+class GenericEntitiesEditView(EntityInstanceMixin, View):
     def get(self, request, *args, **kwargs):
-        entity = kwargs["entity"]
-        pk = kwargs["pk"]
-        entity_model = caching.get_entity_class_of_name(entity)
-        instance = get_object_or_404(entity_model, pk=pk)
         request = set_session_variables(request)
 
         side_bar = []
 
         triples_related_all = (
-            TempTriple.objects_inheritance.filter(Q(subj__pk=pk) | Q(obj__pk=pk))
+            TempTriple.objects_inheritance.filter(
+                Q(subj__pk=self.pk) | Q(obj__pk=self.pk)
+            )
             .all()
             .select_subclasses()
         )
@@ -59,17 +58,17 @@ class GenericEntitiesEditView(View):
             triples_related_by_entity = triples_related_all.filter(
                 (
                     Q(**{f"subj__self_contenttype": entity_content_type})
-                    & Q(**{f"obj__pk": pk})
+                    & Q(**{f"obj__pk": self.pk})
                 )
                 | (
                     Q(**{f"obj__self_contenttype": entity_content_type})
-                    & Q(**{f"subj__pk": pk})
+                    & Q(**{f"subj__pk": self.pk})
                 )
             )
 
             table_class = get_generic_triple_table(
                 other_entity_class_name=other_entity_class_name,
-                entity_pk_self=pk,
+                entity_pk_self=self.pk,
                 detail=False,
             )
 
@@ -83,13 +82,13 @@ class GenericEntitiesEditView(View):
                 (
                     title_card,
                     tb_object,
-                    f"triple_form_{entity}_to_{other_entity_class_name}",
+                    f"triple_form_{self.entity}_to_{other_entity_class_name}",
                     tb_object_open,
                 )
             )
-        form = get_entities_form(entity.title())
-        form = form(instance=instance)
-        form_text = FullTextForm(entity=entity.title(), instance=instance)
+        form = get_entities_form(self.entity.title())
+        form = form(instance=self.instance)
+        form_text = FullTextForm(entity=self.entity.title(), instance=self.instance)
         if "apis_highlighter" in settings.INSTALLED_APPS:
             form_ann_agreement = SelectAnnotatorAgreement()
         else:
@@ -100,37 +99,39 @@ class GenericEntitiesEditView(View):
             if apis_bibsonomy_texts:
                 apis_bibsonomy.extend(
                     [
-                        f"text_{pk}"
+                        f"text_{self.pk}"
                         for pk in TextType.objects.filter(
                             name__in=apis_bibsonomy_texts
                         ).values_list("pk", flat=True)
-                        if f"text_{pk}" not in apis_bibsonomy
+                        if f"text_{self.pk}" not in apis_bibsonomy
                     ]
                 )
             if isinstance(apis_bibsonomy, list):
                 apis_bibsonomy = "|".join([x.strip() for x in apis_bibsonomy])
         else:
             apis_bibsonomy = False
-        object_revisions = Version.objects.get_for_object(instance)
-        object_lod = Uri.objects.filter(root_object=instance)
-        object_texts, ann_proj_form = get_highlighted_texts(request, instance)
-        object_labels = Label.objects.filter(temp_entity=instance)
-        tb_label = LabelTableEdit(data=object_labels, prefix=entity.title()[:2] + "L-")
+        object_revisions = Version.objects.get_for_object(self.instance)
+        object_lod = Uri.objects.filter(root_object=self.instance)
+        object_texts, ann_proj_form = get_highlighted_texts(request, self.instance)
+        object_labels = Label.objects.filter(temp_entity=self.instance)
+        tb_label = LabelTableEdit(
+            data=object_labels, prefix=self.entity.title()[:2] + "L-"
+        )
         tb_label_open = request.GET.get("PL-page", None)
         # side_bar.append(('Label', tb_label, 'PersonLabel', tb_label_open))
         RequestConfig(request, paginate={"per_page": 10}).configure(tb_label)
         perm = ObjectPermissionChecker(request.user)
         permissions = {
-            "change": perm.has_perm("change_{}".format(entity), instance),
-            "delete": perm.has_perm("delete_{}".format(entity), instance),
-            "create": request.user.has_perm("entities.add_{}".format(entity)),
+            "change": perm.has_perm("change_{}".format(self.entity), self.instance),
+            "delete": perm.has_perm("delete_{}".format(self.entity), self.instance),
+            "create": request.user.has_perm("entities.add_{}".format(self.entity)),
         }
         template = get_template("apis_entities/edit_generic.html")
         context = {
-            "entity_type": entity,
+            "entity_type": self.entity,
             "form": form,
             "form_text": form_text,
-            "instance": instance,
+            "instance": self.instance,
             "right_card": side_bar,
             "object_revisions": object_revisions,
             "object_texts": object_texts,
@@ -140,65 +141,62 @@ class GenericEntitiesEditView(View):
             "apis_bibsonomy": apis_bibsonomy,
             "permissions": permissions,
         }
-        form_merge_with = GenericEntitiesStanbolForm(entity, ent_merge_pk=pk)
+        form_merge_with = GenericEntitiesStanbolForm(self.entity, ent_merge_pk=self.pk)
         context["form_merge_with"] = form_merge_with
         return HttpResponse(template.render(request=request, context=context))
 
     def post(self, request, *args, **kwargs):
-        entity = kwargs["entity"]
-        pk = kwargs["pk"]
-        entity_model = caching.get_entity_class_of_name(entity)
-        instance = get_object_or_404(entity_model, pk=pk)
-        form = get_entities_form(entity.title())
-        form = form(request.POST, instance=instance)
-        form_text = FullTextForm(request.POST, entity=entity.title())
+        form = get_entities_form(self.entity.title())
+        form = form(request.POST, instance=self.instance)
+        form_text = FullTextForm(request.POST, entity=self.entity.title())
         if form.is_valid() and form_text.is_valid():
             entity_2 = form.save()
             form_text.save(entity_2)
             return redirect(
                 reverse(
                     "apis:apis_entities:generic_entities_edit_view",
-                    kwargs={"pk": pk, "entity": entity},
+                    kwargs={"pk": self.pk, "entity": self.entity},
                 )
             )
         else:
             template = get_template("apis_entities/edit_generic.html")
             perm = ObjectPermissionChecker(request.user)
             permissions = {
-                "change": perm.has_perm("change_{}".format(entity), instance),
-                "delete": perm.has_perm("delete_{}".format(entity), instance),
-                "create": request.user.has_perm("entities.add_{}".format(entity)),
+                "change": perm.has_perm("change_{}".format(self.entity), self.instance),
+                "delete": perm.has_perm("delete_{}".format(self.entity), self.instance),
+                "create": request.user.has_perm("entities.add_{}".format(self.entity)),
             }
             context = {
                 "form": form,
-                "entity_type": entity,
+                "entity_type": self.entity,
                 "form_text": form_text,
-                "instance": instance,
+                "instance": self.instance,
                 "permissions": permissions,
             }
-            if entity.lower() != "place":
-                form_merge_with = GenericEntitiesStanbolForm(entity, ent_merge_pk=pk)
+            if self.entity.lower() != "place":
+                form_merge_with = GenericEntitiesStanbolForm(
+                    self.entity, ent_merge_pk=self.pk
+                )
                 context["form_merge_with"] = form_merge_with
                 return TemplateResponse(request, template, context=context)
             return HttpResponse(template.render(request=request, context=context))
 
 
 @method_decorator(login_required, name="dispatch")
-class GenericEntitiesCreateView(View):
+class GenericEntitiesCreateView(EntityMixin, View):
     def get(self, request, *args, **kwargs):
-        entity = kwargs["entity"]
-        form = get_entities_form(entity.title())
+        form = get_entities_form(self.entity.title())
         form = form()
-        form_text = FullTextForm(entity=entity.title())
+        form_text = FullTextForm(entity=self.entity.title())
         permissions = {
-            "create": request.user.has_perm("entities.add_{}".format(entity))
+            "create": request.user.has_perm("entities.add_{}".format(self.entity))
         }
         template = get_template("apis_entities/edit_generic.html")
         return HttpResponse(
             template.render(
                 request=request,
                 context={
-                    "entity_type": entity,
+                    "entity_type": self.entity,
                     "permissions": permissions,
                     "form": form,
                     "form_text": form_text,
@@ -207,22 +205,23 @@ class GenericEntitiesCreateView(View):
         )
 
     def post(self, request, *args, **kwargs):
-        entity = kwargs["entity"]
-        form = get_entities_form(entity.title())
+        form = get_entities_form(self.entity.title())
         form = form(request.POST)
-        form_text = FullTextForm(request.POST, entity=entity.title())
+        form_text = FullTextForm(request.POST, entity=self.entity.title())
         if form.is_valid() and form_text.is_valid():
             entity_2 = form.save()
             form_text.save(entity_2)
             return redirect(
                 reverse(
                     "apis:apis_entities:generic_entities_detail_view",
-                    kwargs={"pk": entity_2.pk, "entity": entity},
+                    kwargs={"pk": entity_2.pk, "entity": self.entity},
                 )
             )
         else:
             permissions = {
-                "create": request.user.has_perm("apis_entities.add_{}".format(entity))
+                "create": request.user.has_perm(
+                    "apis_entities.add_{}".format(self.entity)
+                )
             }
             template = get_template("apis_entities/edit_generic.html")
             return HttpResponse(
@@ -238,16 +237,15 @@ class GenericEntitiesCreateView(View):
 
 
 @method_decorator(login_required, name="dispatch")
-class GenericEntitiesCreateStanbolView(View):
+class GenericEntitiesCreateStanbolView(EntityMixin, View):
     def post(self, request, *args, **kwargs):
-        entity = kwargs["entity"]
         ent_merge_pk = kwargs.get("ent_merge_pk", False)
         if ent_merge_pk:
             form = GenericEntitiesStanbolForm(
-                entity, request.POST, ent_merge_pk=ent_merge_pk
+                self.entity, request.POST, ent_merge_pk=ent_merge_pk
             )
         else:
-            form = GenericEntitiesStanbolForm(entity, request.POST)
+            form = GenericEntitiesStanbolForm(self.entity, request.POST)
         # form = form(request.POST)
         if form.is_valid():
             entity_2 = form.save()
@@ -256,12 +254,14 @@ class GenericEntitiesCreateStanbolView(View):
             return redirect(
                 reverse(
                     "apis:apis_entities:generic_entities_edit_view",
-                    kwargs={"pk": entity_2.pk, "entity": entity},
+                    kwargs={"pk": entity_2.pk, "entity": self.entity},
                 )
             )
         else:
             permissions = {
-                "create": request.user.has_perm("apis_entities.add_{}".format(entity))
+                "create": request.user.has_perm(
+                    "apis_entities.add_{}".format(self.entity)
+                )
             }
             template = get_template("apis_entities/edit_generic.html")
             return HttpResponse(
@@ -272,7 +272,7 @@ class GenericEntitiesCreateStanbolView(View):
 
 
 @method_decorator(login_required, name="dispatch")
-class GenericEntitiesDeleteView(DeleteView):
+class GenericEntitiesDeleteView(EntityInstanceMixin, DeleteView):
     # model = ContentType.objects.get(
     #     app_label='apis_entities', model='tempentityclass').model_class()
     model = importlib.import_module("apis_core.apis_entities.models").TempEntityClass
@@ -281,24 +281,23 @@ class GenericEntitiesDeleteView(DeleteView):
     )
 
     def dispatch(self, request, *args, **kwargs):
-        entity = kwargs["entity"]
         self.success_url = reverse(
-            "apis_core:apis_entities:generic_entities_list", kwargs={"entity": entity}
+            "apis_core:apis_entities:generic_entities_list",
+            kwargs={"entity": self.entity},
         )
         return super(GenericEntitiesDeleteView, self).dispatch(request, *args, **kwargs)
 
 
 @method_decorator(login_required, name="dispatch")
-class GenericEntitiesDuplicateView(View):
+class GenericEntitiesDuplicateView(EntityInstanceMixin, View):
     def get(self, request, *args, **kwargs):
-        entity_model = caching.get_entity_class_of_name(kwargs["entity"])
-        source_obj = get_object_or_404(entity_model, pk=kwargs["pk"])
+        source_obj = get_object_or_404(self.entity_model, pk=self.pk)
 
         newobj = source_obj.duplicate()
 
         return redirect(
             reverse(
                 "apis:apis_entities:generic_entities_edit_view",
-                kwargs={"pk": newobj.id, "entity": kwargs["entity"]},
+                kwargs={"pk": newobj.id, "entity": self.entity},
             )
         )
