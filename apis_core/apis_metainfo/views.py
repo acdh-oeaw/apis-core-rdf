6fd75035f95558a4cff4953526897800ec2df030
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-05-17 14:42:23 +0200

    feat: Implement form, view and route for adding URIs
    This commit introduces a form that gets or creates an URI. It creates a
    view and the corresponding route for the view as well as a template for
    the view.
    There is also a subtemplate `gnd_autocomplete_form.html` which is
    included in the URI get_or_create form - it provides an autocomplete
    form that searches for entities in GND and on select adds the
    corresponding URI to the input field.

--- apis_core/apis_metainfo/views.py
+++ apis_core/apis_metainfo/views.py
@@ -1,13 +1,17 @@
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import DeleteView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.views import View
 
 from .browsingviews import GenericListView, BaseCreateView, BaseUpdateView
 from .filters import UriListFilter
-from .forms import UriFilterFormHelper, UriForm
+from .forms import UriFilterFormHelper, UriForm, UriGetOrCreateForm
 from .models import Uri
 from .tables import UriTable
+from apis_core.utils.rdf import get_modelname_and_dict_from_uri
+from apis_core.utils.normalize import clean_uri
 
 
 class UriListView(GenericListView):
@@ -42,3 +46,13 @@ class UriDelete(LoginRequiredMixin, DeleteView):
     model = Uri
     template_name = "confirm_delete.html"
     success_url = reverse_lazy("apis_core:apis_metainfo:uri_browse")
+
+
+class UriGetOrCreate(FormView):
+    template_name = "uri_create.html"
+    form_class = UriGetOrCreateForm
+
+    def form_valid(self, form):
+        if form.is_valid():
+            self.success_url = form.uriobj.root_object.get_absolute_url()
+        return super().form_valid(form)
