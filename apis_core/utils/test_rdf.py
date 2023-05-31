3d4b1e77bd15395b370519718dbe477b0eb93ff1
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-05-31 10:54:27 +0200

    fix(utils): use local files for rdf parser to circument http timeouts
    Closes: #174

--- apis_core/utils/test_rdf.py
+++ apis_core/utils/test_rdf.py
@@ -1,9 +1,15 @@
 # SPDX-FileCopyrightText: 2023 Birger Schacht
 # SPDX-License-Identifier: MIT
 
+from pathlib import Path
+
 from django.test import TestCase
 
 from apis_core.utils import rdf
+
+# use `curl -H "Accept: application/rdf+xml" -L $URI` to fetch data
+
+testdata = Path(__file__).parent / "testdata"
 
 
 class RdfTest(TestCase):
@@ -15,14 +21,16 @@ class RdfTest(TestCase):
             "name": "Achensee",
             "parent": "https://sws.geonames.org/2782113/",
         }
-        uri = "https://www.geonames.org/2783029/achensee.html"
+        # https://www.geonames.org/2783029/achensee.html
+        uri = str(testdata / "achensee.rdf")
         modelname, attributes = rdf.get_modelname_and_dict_from_uri(uri)
         self.assertEqual(modelname, "apis_ontology.Place")
         self.assertEqual(achensee, attributes)
 
     def test_get_modelname_from_dict_place_from_dnb(self):
         wien = {"name": "Wien", "lat": "048.208199", "lon": "016.371690"}
-        uri = "https://d-nb.info/gnd/4066009-6"
+        # https://d-nb.info/gnd/4066009-6
+        uri = str(testdata / "wien.rdf")
         modelname, attributes = rdf.get_modelname_and_dict_from_uri(uri)
         self.assertEqual(modelname, "apis_ontology.Place")
         self.assertEqual(wien, attributes)
@@ -35,7 +43,8 @@ class RdfTest(TestCase):
             "date_of_death": "1942",
             "place_of_birth": "https://d-nb.info/gnd/4066009-6",
         }
-        uri = "https://d-nb.info/gnd/118833197"
+        # https://d-nb.info/gnd/118833197
+        uri = str(testdata / "ramus.rdf")
         modelname, attributes = rdf.get_modelname_and_dict_from_uri(uri)
         self.assertEqual(modelname, "apis_ontology.Person")
         self.assertEqual(pierre, attributes)
@@ -46,7 +55,8 @@ class RdfTest(TestCase):
             "altName": "Pierre Ramus-Gesellschaft",
             "place": "https://d-nb.info/gnd/4066009-6",
         }
-        uri = "https://d-nb.info/gnd/415006-5"
+        # https://d-nb.info/gnd/415006-5
+        uri = str(testdata / "ramus_gesellschaft.rdf")
         modelname, attributes = rdf.get_modelname_and_dict_from_uri(uri)
         self.assertEqual(modelname, "apis_ontology.Institution")
         self.assertEqual(pierre_ges, attributes)
@@ -59,7 +69,8 @@ class RdfTest(TestCase):
             "start_date_written": "1919",
             "end_date_written": "1947",
         }
-        uri = "https://d-nb.info/gnd/35077-1"
+        # https://d-nb.info/gnd/35077-1
+        uri = str(testdata / "oeaw.rdf")
         modelname, attributes = rdf.get_modelname_and_dict_from_uri(uri)
         self.assertEqual(modelname, "apis_ontology.Institution")
         self.assertEqual(pierre_ges, attributes)
