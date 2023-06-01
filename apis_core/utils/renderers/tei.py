199f2f453dab020f556dcb7c8b6b96e17a96e0d7
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-06-01 12:29:41 +0200

    feat(apis_entities,utils): introduce new tei renderer
    This commit replaces the removed `apis_tei` module with a new renderer.
    The renderer is using the template engine to create tei xml files. This
    is done by introducing a default TeiRenderer, that uses templates to
    return xml responses.
    The existing EntityToTEI renderer now inherits from this renderer and
    uses the entites type (person, place, institution) to set the template
    name. A couple of defaults templates are shipped in the
    `apis_entities/templates/apis_entities/tei` folder.

---
+++ apis_core/utils/renderers/tei.py
@@ -0,0 +1,18 @@
+from django.template import loader
+from django.core.exceptions import ImproperlyConfigured
+from rest_framework import renderers
+
+
+class TeiRenderer(renderers.BaseRenderer):
+    media_type = "text/xml"
+    format = "tei"
+    template_name = None
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        if self.template_name:
+            template = loader.get_template(self.template_name)
+            request = renderer_context["request"]
+            return template.render(data, request=request)
+        raise ImproperlyConfigured(
+            "Returned a template response with no `template_name` attribute set on the object"
+        )
