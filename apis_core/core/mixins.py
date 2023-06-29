da2bc4dc71c0b1f99abbebe5687381ced3252036
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-06-29 09:36:22 +0200

    feat(core): Introduce ViewPassesTestMixin and ListViewObjectFilterMixin
    The ViewPassesTestMixin and ListViewObjectFilterMixin are meant to
    provide an easy way of customizing access to objects:
    
    * ViewPassesTestMixin: this mixin is similar to the
      UserPassesTestMixin - its `test_func` looks if there is a custom
      method defined in `settings.APIS_VIEW_PASSES_TEST` and if so uses that
      function to calculate access permissions. If there is no such
      function, it falls back to the existing `access_for_all` check.
    
    * ListViewObjectFilterMixin: this mixin is meant to be included in views
      working with querysets. It allows to define a function in
      `settings.LIST_VIEW_OBJECT_FILTER` and uses that function to filter
      the objects in this view.

---
+++ apis_core/core/mixins.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.conf import settings
+from apis_core.utils.utils import access_for_all
+
+
+class ViewPassesTestMixin(AccessMixin):
+    """
+    Deny a request with a permission error if the test_func() method returns
+    False. This is mostly a copy of Django's UserPassesTestMixin, but it allows
+    to set the APIS_VIEW_PASSES_TEST setting to define a function that receives
+    the view and can perform checks on any of the views attributes.
+    If such a setting does not exist, it falls back  to APIS' access_for_all
+    function.
+    """
+
+    def test_func(self):
+        if hasattr(settings, "APIS_VIEW_PASSES_TEST"):
+            return settings.APIS_VIEW_PASSES_TEST(self)
+        # fall back to more general access check
+        return access_for_all(self, viewtype="detail")
+
+    def get_test_func(self):
+        """
+        Override this method to use a different test_func method.
+        """
+        return self.test_func
+
+    def dispatch(self, request, *args, **kwargs):
+        view_test_result = self.get_test_func()()
+        if not view_test_result:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ListViewObjectFilterMixin:
+    """
+    Filter a queryset of a listview using the APIS_LIST_VIEW_OBJECT_FILTER
+    setting if it exists. A child class has to call the `filter_queryset`
+    method somewhere, most likely in the `get_queryset` method.
+    """
+
+    def filter_queryset(self, queryset):
+        if hasattr(settings, "APIS_LIST_VIEW_OBJECT_FILTER"):
+            return settings.APIS_LIST_VIEW_OBJECT_FILTER(queryset)
+        return queryset
