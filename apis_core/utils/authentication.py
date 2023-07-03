b9dcc1ac8ffa744be2695f0533e311ca42d0a8e6
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-07-03 18:11:38 +0200

    refactor: delete unused files and code
    The files in apis_entities/
    * `autocomplete_light_registry.py`
    * `custom_autocompletes.py`
    are not being used anywhere
    
    `apis_entities/custom_authentication.py` is not being used, but could be
    used by downstream projects. It was moved to
    `apis_core.utils.authentication.py`.
    
    The files in apis_relations/
    * `autocomplete_light_registry.py`
    * `serializers.py`
    are not being used anywhere
    
    The file `apis_relations/forms.py` was only used in
    `apis_relations/views.py` and in there the result of the function that
    used it was assigned to a variable that was not used anywhere. The whole
    code block is now deleted.

--- apis_core/utils/authentication.py
+++ apis_core/utils/authentication.py
@@ -1,10 +1,11 @@
 from rest_framework.authentication import TokenAuthentication
 
 
+# not used in APIS, but can be used in downstream projects
 class TokenAuthSupportQueryString(TokenAuthentication):
     """
     Extend the TokenAuthentication class to support querystring authentication
-    in the form of "http://www.example.com/?auth_token=<token_key>"
+    in the form of "http://www.example.com/?auth_token=<api_key>"
     """
 
     def authenticate(self, request):
@@ -16,4 +17,4 @@ class TokenAuthSupportQueryString(TokenAuthentication):
         ):
             return self.authenticate_credentials(request.query_params.get("api_key"))
         else:
-            return super(TokenAuthSupportQueryString, self).authenticate(request)
+            return super().authenticate(request)
