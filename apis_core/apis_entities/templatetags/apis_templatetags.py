9334360e3b7498aaef26c1196fab6d780bafc54b
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-02 16:21:01 +0200

    feat: introduce new templatetags
    This commit introduces a core templatetag `shared_url` to access the
    `SHARED_URL` setting (the templatetag defaults to `/static/`). It also
    updates the base template to use this templatetag.
    
    The commit also introduces an apis_entities templatetag
    `entities_list_links` that returns a list of links to the list views of
    all entities. It also updates the base template to use this templatetag.
    
    Closes: #293

--- apis_core/apis_entities/templatetags/apis_templatetags.py
+++ apis_core/apis_entities/templatetags/apis_templatetags.py
@@ -1,4 +1,6 @@
+from operator import itemgetter
 from django import template
+from apis_core.utils import caching
 
 register = template.Library()
 
@@ -15,3 +17,18 @@ def url_replace(request, field, value):
     dict_[field] = value
 
     return dict_.urlencode()
+
+
+@register.simple_tag
+def entities_list_links():
+    """
+    Retrieve all models which inherit from AbstractEntity class
+    and return their class name and verbose name.
+    """
+    entities_classes = caching.get_all_entity_classes() or []
+    entities_links = [
+        (e.__name__.lower(), e._meta.verbose_name.title()) for e in entities_classes
+    ]
+    entities_links.sort(key=itemgetter(1))
+
+    return entities_links
