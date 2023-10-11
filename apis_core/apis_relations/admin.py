4e761f40105a6ce3a2668600b61acaae71fd36cd
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-11 12:30:36 +0200

    feat: add Property to admin interface

--- apis_core/apis_relations/admin.py
+++ apis_core/apis_relations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from apis_core.apis_relations.models import Triple
+from apis_core.apis_relations.models import Triple, Property
 
 admin.site.register(Triple)
+admin.site.register(Property)
