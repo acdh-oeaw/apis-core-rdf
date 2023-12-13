e2aa6a3ea27787c23a80ea9f6f853b01db98393a
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-12-13 19:40:33 +0100

    feat: improve listview pagination
    This improves the pagination links below the table in the generic list
    view. to make the template code smaller, we introducce a templatetag
    `page_range` that uses the paginators get_elided_page_range to get a
    list of useful pagination links.
    
    Closes: #416

--- apis_core/core/templatetags/apiscore.py
+++ apis_core/core/templatetags/apiscore.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.simple_tag
 def shared_url():
     return getattr(settings, "SHARED_URL", "/static/")
+
+
+@register.simple_tag
+def page_range(paginator, number):
+    return paginator.get_elided_page_range(number=number)
