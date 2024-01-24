45de48bea2b81823873066820a079635b8d7a04a
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-24 13:19:26 +0100

    refactor(apis_entities)!: use new generic app
    This commit drops all the existing entities views and recreates them
    using the `generic` app as a basis.
    
    Additionaly, it adds a merge view and a merge form.
    
    The commit deletes the following files, because they are not used
    anymore:
    * detail_generic.py, edit_generic.py: those are now handled by
      generic.views and apis_entities.views
    * merge_views.py: replaced by a new merge view in apis_entities.views
    * filters.py, tables.py: were only used by GenericListViewNew, which is
      now replaced by generic.views.List
    
    BREAKING CHANGE: with the removal of `filters.py` and `tables.py` your
    configuration in `APIS_ENTITIES` won't have any effect anymore. Please
    replace them with custom tables and custom filtersets. Also the the
    locations and names and structure of the templates changed, so you will
    have to update those.

--- apis_core/apis_entities/tables.py
+++ apis_core/apis_entities/tables.py
@@ -1,70 +1,20 @@
-import django_tables2 as tables
-from django_tables2.utils import A
-from apis_core.utils.settings import list_links_to_edit
-
-from apis_core.apis_metainfo.tables import (
-    generic_order_start_date_written,
-    generic_order_end_date_written,
-    generic_render_start_date_written,
-    generic_render_end_date_written,
-)
-from apis_core.utils import caching
+from apis_core.generic.tables import GenericTable, ActionColumn
 
 
-def get_entities_table(entity, default_cols):
-    if default_cols is None:
-        default_cols = [
-            "name",
-        ]
+class DuplicateColumn(ActionColumn):
+    """
+    A column showing a view button
+    """
 
-    class GenericEntitiesTable(tables.Table):
-        def render_name(self, record, value):
-            if value == "":
-                return "(No name provided)"
-            else:
-                return value
+    template_name = "columns/duplicate.html"
 
-        # reuse the logic for ordering and rendering *_date_written
-        # Important: The names of these class variables must correspond to the column field name,
-        # e.g. for start_date_written, the methods must be named order_start_date_written and render_start_date_written
-        order_start_date_written = generic_order_start_date_written
-        order_end_date_written = generic_order_end_date_written
-        render_start_date_written = generic_render_start_date_written
-        render_end_date_written = generic_render_end_date_written
-        if list_links_to_edit():
-            name = tables.LinkColumn(
-                "apis:apis_entities:generic_entities_edit_view",
-                args=[entity.lower(), A("pk")],
-                empty_values=[],
-            )
-        else:
-            name = tables.LinkColumn(
-                "apis:apis_entities:generic_entities_detail_view",
-                args=[entity.lower(), A("pk")],
-                empty_values=[],
-            )
-        export_formats = [
-            "csv",
-            "json",
-            "xls",
-            "xlsx",
-        ]
-        if "id" in default_cols:
-            id = tables.LinkColumn()
 
-        class Meta:
-            model = caching.get_ontology_class_of_name(entity)
-            fields = default_cols
-            attrs = {"class": "table table-hover table-striped table-condensed"}
-            # quick ensurance if column is indeed a field of this entity
-            for col in default_cols:
-                if not hasattr(model, col):
-                    raise Exception(
-                        f'APIS_ENTITIES "table_fields" setting for {entity} entity\n'
-                        f'references "{col}" field, which model class does not possess.\n'
-                    )
+class AbstractEntityTable(GenericTable):
+    noduplicate = DuplicateColumn()
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        # if there is no custom sequence set, move `edit` and `delete` to the back
+        if "sequence" not in kwargs:
+            kwargs["sequence"] = ["...", "view", "edit", "noduplicate", "delete"]
 
-    return GenericEntitiesTable
+        super().__init__(*args, **kwargs)
--- apis_core/apis_entities/urls.py
+++ apis_core/apis_entities/urls.py
@@ -1,61 +1,88 @@
-from django.urls import include, path
+from django.contrib.contenttypes.models import ContentType
+from django.http import Http404
+from django.urls import include, path, register_converter
+from django.shortcuts import get_list_or_404
 
-from . import views, edit_generic, detail_generic, merge_views
 from .autocomplete3 import (
     GenericEntitiesAutocomplete,
     GenericNetworkEntitiesAutocomplete,
 )
 
 # from .views import ReversionCompareView TODO: add again when import is fixed
-from .edit_generic import GenericEntitiesCreateStanbolView
 from .api_views import GetOrCreateEntity
+from apis_core.apis_entities.models import AbstractEntity
+from apis_core.generic.views import List, Create, Delete, Detail
+from apis_core.apis_entities.views import (
+    EntitiesDuplicate,
+    EntitiesUpdate,
+    EntitiesMerge,
+)
+
+
+class EntityToContenttypeConverter:
+    """
+    A converter that converts from a the name of an entity class
+    (i.e. `person`) to the actual Django model class.
+    """
+
+    regex = r"\w+"
+
+    def to_python(self, value):
+        candiates = get_list_or_404(ContentType, model=value)
+        candiates = list(
+            filter(lambda ct: issubclass(ct.model_class(), AbstractEntity), candiates)
+        )
+        if len(candiates) > 1:
+            raise Http404("Multiple entities match the <%s> identifier" % value)
+        return candiates[0]
+
+    def to_url(self, value):
+        return value
+
+
+register_converter(EntityToContenttypeConverter, "entitytocontenttype")
 
 app_name = "apis_entities"
 
 entity_patterns = [
     path(
         "list/",
-        views.GenericListViewNew.as_view(),
+        List.as_view(),
         name="generic_entities_list",
     ),
     path(
         "create/",
-        edit_generic.GenericEntitiesCreateView.as_view(),
+        Create.as_view(),
         name="generic_entities_create_view",
     ),
     path(
         "<int:pk>/detail/",
-        detail_generic.GenericEntitiesDetailView.as_view(),
+        Detail.as_view(),
         name="generic_entities_detail_view",
     ),
     path(
         "<int:pk>/edit/",
-        edit_generic.GenericEntitiesEditView.as_view(),
+        EntitiesUpdate.as_view(),
         name="generic_entities_edit_view",
     ),
     path(
         "<int:pk>/delete/",
-        edit_generic.GenericEntitiesDeleteView.as_view(),
+        Delete.as_view(),
         name="generic_entities_delete_view",
     ),
     path(
         "<int:pk>/duplicate/",
-        edit_generic.GenericEntitiesDuplicateView.as_view(),
+        EntitiesDuplicate.as_view(),
         name="generic_entities_duplicate_view",
+    ),
+    path(
+        "<int:pk>/merge/",
+        EntitiesMerge.as_view(),
+        name="generic_entities_merge_view",
     ),
 ]
 
 autocomplete_patterns = [
-    path(
-        "createstanbol/<slug:entity>/<int:ent_merge_pk>/",
-        GenericEntitiesCreateStanbolView.as_view(),
-        name="generic_entities_stanbol_create",
-    ),
-    path(
-        "createstanbol/<slug:entity>/",
-        GenericEntitiesCreateStanbolView.as_view(),
-        name="generic_entities_stanbol_create",
-    ),
     path(
         "<slug:entity>/<int:ent_merge_pk>/",
         GenericEntitiesAutocomplete.as_view(),
@@ -75,7 +102,7 @@ autocomplete_patterns = [
 
 urlpatterns = [
     path(
-        "entity/<slug:entity>/",
+        "entity/<entitytocontenttype:contenttype>/",
         include(entity_patterns),
     ),
     path(
@@ -87,7 +114,6 @@ urlpatterns = [
         GenericNetworkEntitiesAutocomplete.as_view(),
         name="generic_network_entities_autocomplete",
     ),
-    path("merge-objects/", merge_views.merge_objects, name="merge_objects"),
     path(
         "getorcreateentity/",
         GetOrCreateEntity.as_view(),
