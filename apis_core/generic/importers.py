06ad57b629c912f9419024e77bc4aff299a518fe
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-27 18:23:36 +0100

    feat(generic): implement extendable importer
    This commit introduces the logic for implementing extendable importers.
    It also adds a simple `import` endpoint that uses the autocomplete
    endpoint to list *external* resources and then tries to import them.
    
    Closes: #541

---
+++ apis_core/generic/importers.py
@@ -0,0 +1,49 @@
+import json
+import urllib
+from django.core.exceptions import ImproperlyConfigured
+from apis_core.utils.normalize import clean_uri
+
+
+class GenericImporter:
+    """
+    A skeleton importer class
+    It provides the standard methods for importing data from
+    an URI and creating a model instance of it.
+    By default it fetches a resource, tries to parse it using json and
+    then extracts the fields whose keys match the model field names.
+    Projects can inherit from this class and override the default
+    methods or simple write their own from scratch.
+    """
+
+    model = None
+    import_uri = None
+
+    def __init__(self, uri, model):
+        self.model = model
+        self.import_uri = self.clean_uri(uri)
+
+    @property
+    def get_uri(self):
+        return self.import_uri
+
+    def clean_uri(self, uri):
+        return clean_uri(uri)
+
+    def request(self, uri):
+        try:
+            return json.loads(urllib.request.urlopen(uri).read())
+        except Exception:
+            return {}
+
+    def mangle_data(self, data):
+        return data
+
+    def create_instance(self):
+        data = self.request(self.import_uri)
+        data = self.mangle_data(data)
+        # we are dropping all fields that are not part of the model
+        modelfields = [field.name for field in self.model._meta.fields]
+        data = {key: data[key] for key in data if key in modelfields}
+        if data:
+            return self.model.objects.create(**data)
+        raise ImproperlyConfigured
