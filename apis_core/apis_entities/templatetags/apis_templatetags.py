d913467fa79f51a495e0e89a293dcab16d6aa555
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-03-15 11:43:14 +0100

    refactor(apis_entities): drop unused templatetag and template

--- apis_core/apis_entities/templatetags/apis_templatetags.py
+++ apis_core/apis_entities/templatetags/apis_templatetags.py
@@ -6,12 +6,6 @@ from apis_core.utils.helpers import triple_sidebar
 register = template.Library()
 
 
-@register.inclusion_tag("apis_entities/apis_create_entities.html", takes_context=True)
-def apis_create_entities(context):
-    values = {}
-    return values
-
-
 @register.simple_tag
 def url_replace(request, field, value):
     dict_ = request.GET.copy()
