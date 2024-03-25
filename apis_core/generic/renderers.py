b60a0e5ac7d86186eb4e8cf2b533e4dc69fb9d04
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-03-25 12:21:27 +0100

    feat(generic): ship generic RDF renderers
    They can be enabled as described in the upstream DRF framework
    documentation:
    https://www.django-rest-framework.org/api-guide/renderers/#setting-the-renderers
    They expect the renderer to put triples in the data object and they
    build the graph based on these triples.

---
+++ apis_core/generic/renderers.py
@@ -0,0 +1,32 @@
+from rest_framework import renderers
+from rdflib import Graph
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class GenericRDFBaseRenderer(renderers.BaseRenderer):
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        g = Graph()
+        for result in data.get("results", []):
+            match result:
+                case tuple(_, _, _):
+                    g.add(result)
+                case other:
+                    logger.debug("Could not add %s to RDF graph: not a tuple", other)
+        return g.serialize(format=self.media_type)
+
+
+class GenericRDFXMLRenderer(GenericRDFBaseRenderer):
+    media_type = "application/rdf+xml"
+    format = "rdf+xml"
+
+
+class GenericRDFTurtleRenderer(GenericRDFBaseRenderer):
+    media_type = "text/turtle"
+    format = "rdf+turtle"
+
+
+class GenericRDFN3Renderer(GenericRDFBaseRenderer):
+    media_type = "text/n3"
+    format = "rdf+n3"
