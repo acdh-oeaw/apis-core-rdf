f0364453f8a9ebe1d069cf6498db5d59770e7fb4
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-03-27 13:42:42 +0100

    fix(generic): let the importer give more meaningfull error messages

--- apis_core/generic/forms/fields.py
+++ apis_core/generic/forms/fields.py
@@ -1,10 +1,17 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelChoiceField
+from django.utils.translation import gettext as _
 from apis_core.utils.helpers import create_object_from_uri
 
 
 class ModelImportChoiceField(ModelChoiceField):
     def to_python(self, value):
-        result = create_object_from_uri(value, self.queryset.model)
-        if result is not None:
-            return result
-        return super().to_python(value)
+        result = None
+        try:
+            result = create_object_from_uri(value, self.queryset.model)
+        except Exception as e:
+            raise ValidationError(
+                _("Could not import %(value)s: %(exception)s"),
+                params={"value": value, "exception": e},
+            )
+        return result or super().to_python(value)
