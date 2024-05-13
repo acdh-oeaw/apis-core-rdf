742bb1bb7c3f054a90cf10f9b280445e46d22bc0
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-05-13 14:17:05 +0200

    fix: add option to skip simple history tests if `CI` env is set

--- apis_core/history/tests/test_simple_history.py
+++ apis_core/history/tests/test_simple_history.py
@@ -1,44 +1,52 @@
+import os
+import unittest
 from apis_core.apis_relations.models import Property
 from django.test import TestCase
 from django.contrib.contenttypes.models import ContentType
-from apis_ontology.models import Person, Place, Profession
 from apis_core.apis_relations.models import TempTriple
 from datetime import datetime, timedelta
 
 
+@unittest.skipIf(os.environ.get("CI", False), "Skip test if running CI/CD.")
 class SimpleHistoryTestCase(TestCase):
     """Test of the simple_history package for the ÖBL usecase.
     This expects the ÖBL ontology to be installed and migrated: https://github.com/acdh-oeaw/apis-instance-oebl-pnp
     """
 
     def setUp(self):
+        from apis_ontology.models import Person, Place, Profession
+
+        self.Person = Person
+        self.Place = Place
+        self.Profession = Profession
+
         prop = Property.objects.create(
             name_forward="geboren in", name_reverse="Geburtsort von"
         )
         prop.subj_class.add(ContentType.objects.get(model="person"))
         prop.obj_class.add(ContentType.objects.get(model="place"))
-        Person.objects.create(first_name="John", name="Doe")
+        Person.objects.create(forename="John", surname="Doe")
         Place.objects.create(
-            name="Steyr", _history_date=datetime.now() - timedelta(hours=0, minutes=50)
+            label="Steyr", _history_date=datetime.now() - timedelta(hours=0, minutes=50)
         )
 
     def test_history(self):
         """Tests the simple version of attributes changed on a models instance."""
-        pers = Person.objects.get(first_name="John")
-        pers.first_name = "Jane"
+        pers = self.Person.objects.get(forename="John")
+        pers.forename = "Jane"
         pers.save()
-        self.assertEqual(pers.first_name, "Jane")
+        self.assertEqual(pers.forename, "Jane")
         pers_history = pers.history.all()
         assert len(pers_history) == 2
-        self.assertEqual("Jane", pers.history.most_recent().first_name)
-        self.assertEqual("John", pers.history.earliest().first_name)
+        self.assertEqual("Jane", pers.history.most_recent().forename)
+        self.assertEqual("John", pers.history.earliest().forename)
 
     def test_history_through_triples(self):
         """Tests the newly introduced function for retrieving triples for a specific version of a model instance."""
         # first entity
-        pers = Person.objects.get(first_name="John")
+        pers = self.Person.objects.get(forename="John")
         # second entity
-        place = Place.objects.first()
+        place = self.Place.objects.first()
         # create a triple
         tt1 = TempTriple.objects.create(
             subj=pers, prop=Property.objects.first(), obj=place
@@ -53,7 +61,7 @@ class SimpleHistoryTestCase(TestCase):
         self.assertEqual(len(triples), 1)
         self.assertEqual(triples[0].start_date_written, "2020-01-01")
         # change the place and test again
-        place2 = Place.objects.create(name="testplace")
+        place2 = self.Place.objects.create(label="testplace")
         tt1.obj = place2
         tt1.save()
         triples = place.history.earliest().get_triples_for_version()
@@ -74,15 +82,15 @@ class SimpleHistoryTestCase(TestCase):
 
     def test_history_tag(self):
         """Tests the version tag function."""
-        pers = Person.objects.get(first_name="John")
+        pers = self.Person.objects.get(forename="John")
         pers.history.latest().set_version_tag("test_tag")
         self.assertEqual(pers.history.latest().version_tag, "test_tag")
         pers_history = pers.history.all()
         self.assertEqual(pers_history.count(), 1)
         self.assertEqual(pers_history[0].version_tag, "test_tag")
         # test with temTriple
-        pers2 = Person.objects.create(first_name="Jane", name="Doe")
-        place = Place.objects.first()
+        pers2 = self.Person.objects.create(forename="Jane", surname="Doe")
+        place = self.Place.objects.first()
         TempTriple.objects.create(subj=pers2, prop=Property.objects.first(), obj=place)
         pers2.history.latest().set_version_tag("test_tag")
         self.assertEqual(pers2.history.latest().version_tag, "test_tag")
@@ -101,31 +109,31 @@ class SimpleHistoryTestCase(TestCase):
 
     def test_history_delete_entry(self):
         """Tests the deletion of an entry."""
-        pers = Person.objects.get(first_name="John")
+        pers = self.Person.objects.get(forename="John")
         pers.delete()
-        assert len(Person.history.all()) == 2
+        assert len(self.Person.history.all()) == 2
 
     def test_history_merge(self):
         """Tests the merge function of the Place model. This is still expected to fail."""
-        pl1 = Place.objects.first()
-        pl2 = Place.objects.create(
-            name="Test", _history_date=datetime.now() - timedelta(hours=0, minutes=10)
+        pl1 = self.Place.objects.first()
+        pl2 = self.Place.objects.create(
+            label="Test", _history_date=datetime.now() - timedelta(hours=0, minutes=10)
         )
-        pl1.merge_with(pl2)
+        pl1.merge_with([pl2])
         print("save()")
 
     def test_m2m_save(self):
         """Test if m2m profession is saved correctly."""
-        pers = Person.objects.create(
-            first_name="John",
+        pers = self.Person.objects.create(
+            forename="John",
             _history_date=datetime.now() - timedelta(hours=0, minutes=10),
         )
-        self.assertEqual(pers.first_name, "John")
+        self.assertEqual(pers.forename, "John")
         pers_history = pers.history.all()
         assert len(pers_history) == 1
-        prof = Profession.objects.create(name="Test")
+        prof = self.Profession.objects.create(name="Test")
         pers.profession.add(prof)
-        pers.first_name = "Jane"
+        pers.forename = "Jane"
         pers.save()
         assert len(pers.profession.all()) == 1
         assert len(pers.history.latest().profession.all()) == 1
@@ -133,7 +141,7 @@ class SimpleHistoryTestCase(TestCase):
 
     def test_history_date(self):
         """Test that history is set correctly when not manually set."""
-        pers = Person.objects.all().first()
+        pers = self.Person.objects.all().first()
         pers.first_name = "Jane"
         pers.save()
         assert pers.history.earliest().history_date < pers.history.latest().history_date
