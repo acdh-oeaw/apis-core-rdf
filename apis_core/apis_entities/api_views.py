6d77a9d59225a152f7a0c1e773afaadb186c68e5
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-16 12:07:53 +0200

    feat(apis_entities): add endpoint that lists all entities
    Closes: #850

--- apis_core/apis_entities/api_views.py
+++ apis_core/apis_entities/api_views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import redirect
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
+from rest_framework.generics import ListAPIView
 
 from apis_core.apis_metainfo.models import RootObject
+from apis_core.apis_entities.serializers import MinimalEntitySerializer
+from apis_core.apis_entities.utils import get_entity_classes
 
 
 class GetEntityGeneric(APIView):
@@ -12,3 +16,15 @@ class GetEntityGeneric(APIView):
             return redirect(obj.get_api_detail_endpoint())
         except RootObject.DoesNotExist:
             raise NotFound
+
+
+class ListEntityGeneric(ListAPIView):
+    serializer_class = MinimalEntitySerializer
+
+    def get_queryset(self):
+        entities = get_entity_classes()
+        entities = [entity._meta.model_name for entity in entities]
+        q = Q()
+        for entity in entities:
+            q |= Q(**{f"{entity}__isnull": False})
+        return RootObject.objects_inheritance.select_subclasses().filter(q)
---
+++ apis_core/apis_entities/utils.py
@@ -0,0 +1,6 @@
+from django.apps import apps
+from apis_core.apis_entities.models import AbstractEntity
+
+
+def get_entity_classes():
+    return list(filter(lambda x: issubclass(x, AbstractEntity), apps.get_models()))
