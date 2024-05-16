20becad6b84a5691e930e17f25411c747425308a
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-16 15:54:37 +0200

    chore: remove unused files and classes

--- apis_core/apis_metainfo/serializers.py
+++ apis_core/apis_metainfo/serializers.py
@@ -1,27 +1,8 @@
-from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
-from .models import Collection, Uri
+from .models import Uri
 
 from apis_core.generic.serializers import GenericHyperlinkedModelSerializer
-
-
-class CollectionSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name="apis:apis_api:text-detail", lookup_field="pk"
-    )
-    collection_type = serializers.HyperlinkedRelatedField(
-        view_name="apis:apis_api:collectiontype-detail",
-        lookup_field="pk",
-        read_only=True,
-    )
-    parent_class = serializers.HyperlinkedRelatedField(
-        view_name="apis:apis_api:collection-detail", lookup_field="pk", read_only=True
-    )
-
-    class Meta:
-        fields = ["url", "name", "description", "collection_type", "parent_class"]
-        model = Collection
 
 
 class UriSerializer(GenericHyperlinkedModelSerializer):
@@ -32,13 +13,3 @@ class UriSerializer(GenericHyperlinkedModelSerializer):
     class Meta:
         fields = "__all__"
         model = Uri
-
-
-class ContentTypeSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name="apis:apis_api:contenttype-detail", lookup_field="pk"
-    )
-
-    class Meta:
-        fields = "__all__"
-        model = ContentType
