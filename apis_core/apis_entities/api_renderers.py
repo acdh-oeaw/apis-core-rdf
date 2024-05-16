ddc5214dc4ba7fb1ab9b6b09998e03faf8669444
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-16 15:54:37 +0200

    chore: remove unused files and classes

--- apis_core/apis_entities/api_renderers.py
+++
@@ -1,7 +0,0 @@
-from apis_core.utils.renderers.tei import TeiRenderer
-
-
-class EntityToTEI(TeiRenderer):
-    def render(self, data, media_type=None, renderer_context=None):
-        self.template_name = f"apis_entities/tei/{data['entity_type']}.xml"
-        return super().render(data, media_type, renderer_context)
