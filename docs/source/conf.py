9774af2105c3e91b5e11e890eabb99b1df12a804
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-27 09:14:48 +0200

    fix(docs): update settings file of doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sys
 import django
 
 sys.path.insert(0, os.path.abspath("../../"))
-os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings_ci"
+os.environ["DJANGO_SETTINGS_MODULE"] = "sample_project.settings"
 django.setup()
 
 # -- General configuration ------------------------------------------------
