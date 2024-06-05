fb93706ab02839657e46dafa5790e73b4afff621
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-05 15:06:48 +0200

    refactor(apis_entities): replace `get_autocomplete_property_choices`
    The `get_autocomplete_property_choices` was a method in the soon to be
    obsolete `caching` module. This commit moves the method to the
    autocomplete view where it is used and simplifies the method.

--- apis_core/apis_entities/autocomplete3.py
+++ apis_core/apis_entities/autocomplete3.py
@@ -6,12 +6,13 @@ from functools import reduce
 
 from dal import autocomplete
 from django import http
+from django.shortcuts import get_object_or_404
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import FieldError
 from django.db.models import Q
 
-from apis_core.utils.caching import get_autocomplete_property_choices
 from apis_core.utils.settings import get_entity_settings_by_modelname
+from apis_core.apis_relations.models import Property
 
 
 # TODO RDF: Check if this should be removed or adapted
@@ -70,9 +71,42 @@ class PropertyAutocomplete(autocomplete.Select2ListView):
     SELF_SUBJ_OTHER_OBJ_STR = "self_subj_other_obj"
     SELF_OBJ_OTHER_SUBJ_STR = "self_obj_other_subj"
 
+    def get_autocomplete_property_choices(self, self_model, other_model, search_str):
+        self_contenttype = get_object_or_404(ContentType, model=self_model)
+        other_contenttype = get_object_or_404(ContentType, model=other_model)
+
+        rbc_self_subj_other_obj = Property.objects.filter(
+            subj_class=self_contenttype,
+            obj_class=other_contenttype,
+            name_forward__icontains=search_str,
+        )
+        rbc_self_obj_other_subj = Property.objects.filter(
+            subj_class=other_contenttype,
+            obj_class=self_contenttype,
+            name_reverse__icontains=search_str,
+        )
+        choices = []
+        for rbc in rbc_self_subj_other_obj:
+            choices.append(
+                {
+                    # misuse of the id item as explained above
+                    "id": f"id:{rbc.pk}__direction:{self.SELF_SUBJ_OTHER_OBJ_STR}",
+                    "text": rbc.name_forward,
+                }
+            )
+        for rbc in rbc_self_obj_other_subj:
+            choices.append(
+                {
+                    # misuse of the id item as explained above
+                    "id": f"id:{rbc.pk}__direction:{self.SELF_OBJ_OTHER_SUBJ_STR}",
+                    "text": rbc.name_reverse,
+                }
+            )
+        return choices
+
     def get(self, request, *args, **kwargs):
         more = False
-        choices = get_autocomplete_property_choices(
+        choices = self.get_autocomplete_property_choices(
             kwargs["entity_self"], kwargs["entity_other"], self.q
         )
         return http.HttpResponse(
