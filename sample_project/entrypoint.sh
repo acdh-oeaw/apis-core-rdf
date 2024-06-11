93a95b882499003b8378b4f2f036569a1892b382
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-06-11 12:08:24 +0200

    fix: use loaddata specific settings in the entrypoint

--- sample_project/entrypoint.sh
+++ sample_project/entrypoint.sh
@@ -2,5 +2,5 @@
 
 /app/manage.py migrate
 /app/manage.py setup || true
-/app/manage.py loaddata sample_project
+/app/manage.py loaddata sample_project --settings=sample_project.settings_loaddata
 /app/manage.py runserver 0.0.0.0:5000
