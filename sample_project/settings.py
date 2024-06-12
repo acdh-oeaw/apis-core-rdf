071fa2f7bd7a29d23d0c9f1c77f66c849cc717f5
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-12 14:05:22 +0200

    fix(sample_project): allow all hosts

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -4,7 +4,7 @@ from django.core.management.utils import get_random_secret_key
 SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
