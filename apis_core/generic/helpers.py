335bb756d8edf801f7f1c42e1ea43a485a0e2b52
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-13 11:53:12 +0200

    feat(generic): allow passing a prefix to `generate_search_filter`
    This allows us to use the `generate_search_filter` method in loops and
    prepend the filters with model names.

--- apis_core/generic/helpers.py
+++ apis_core/generic/helpers.py
@@ -8,13 +8,16 @@ from django.utils import module_loading
 logger = logging.getLogger(__name__)
 
 
-def generate_search_filter(model, query, fields_to_search=None):
+def generate_search_filter(model, query, fields_to_search=None, prefix=""):
     """
     Generate a default search filter that searches for the `query`
     in all the CharFields and TextFields of a model (case-insensitive)
     or in the fields listed in the `fields_to_search` argument.
     This helper can be used by autocomplete querysets if nothing
     fancier is needed.
+    If the `prefix` is set, the field names will be prefixed with that string -
+    this can be useful if you want to use the `generate_search_filter` in a
+    `Q` combined query while searching over multiple models.
     """
     query = query.split()
 
@@ -33,7 +36,8 @@ def generate_search_filter(model, query, fields_to_search=None):
 
     for token in query:
         q &= functools.reduce(
-            lambda acc, field_name: acc | Q(**{f"{field_name}__icontains": token}),
+            lambda acc, field_name: acc
+            | Q(**{f"{prefix}{field_name}__icontains": token}),
             fields_to_search,
             Q(),
         )
