6211dc73c90ce0933feaf283a2166aae6b0a0bf6
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-17 11:09:56 +0200

    feat(utils): add a NewlineSeparatedListField
    This field is based on the NewlineSeparatedListWidget. It makes it
    possible to use the widget without having to set the widget in the form.

---
+++ apis_core/utils/fields.py
@@ -0,0 +1,17 @@
+from django.db import models
+from apis_core.generic.forms.widgets import NewlineSeparatedListWidget
+
+
+class NewlineSeparatedListField(models.TextField):
+    """
+    This field is basically a textfield with a custom widget.
+    It uses the NewlineSeparatedListWidget to provide a simply way
+    for users to enter multiple values without having to think about
+    the separator.
+    """
+
+    def formfield(self, form_class=None, choices_form_class=None, **kwargs):
+        kwargs["widget"] = NewlineSeparatedListWidget
+        return super().formfield(
+            form_class=form_class, choices_form_class=choices_form_class, **kwargs
+        )
