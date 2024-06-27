d8649d7d907d07f2ef6b3e0def374d52645d46cc
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-27 12:22:34 +0200

    fix(apis_entities): split excluded filters and excluded columns
    We don't want to exclude *all* those fields from the table columns

--- apis_core/apis_entities/filtersets.py
+++ apis_core/apis_entities/filtersets.py
@@ -9,9 +9,12 @@ from apis_core.apis_entities.utils import get_entity_classes
 from apis_core.apis_metainfo.models import RootObject
 from django.contrib.contenttypes.models import ContentType
 
-ABSTRACT_ENTITY_FILTERS_EXCLUDE = [
+ABSTRACT_ENTITY_COLUMNS_EXCLUDE = [
     "rootobject_ptr",
     "self_contenttype",
+]
+
+ABSTRACT_ENTITY_FILTERS_EXCLUDE = ABSTRACT_ENTITY_COLUMNS_EXCLUDE + [
     "review",
     "start_date",
     "start_start_date",
@@ -104,7 +107,7 @@ def related_entity(queryset, name, value):
 
 
 class AbstractEntityFilterSetForm(GenericFilterSetForm):
-    columns_exclude = ABSTRACT_ENTITY_FILTERS_EXCLUDE
+    columns_exclude = ABSTRACT_ENTITY_COLUMNS_EXCLUDE
 
 
 class AbstractEntityFilterSet(GenericFilterSet):
