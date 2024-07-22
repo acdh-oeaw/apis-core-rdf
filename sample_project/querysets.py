8d6d8ddcd5c7def101b471d13f767e2ccf90bfaf
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-07-22 12:04:14 +0200

    fix(sample_project): add autocomplete querysets for person and place
    Closes: #900

---
+++ sample_project/querysets.py
@@ -0,0 +1,41 @@
+import os
+from apis_core.utils.autocomplete import (
+    ExternalAutocomplete,
+    TypeSenseAutocompleteAdapter,
+    LobidAutocompleteAdapter,
+)
+
+
+class PlaceExternalAutocomplete(ExternalAutocomplete):
+    adapters = [
+        TypeSenseAutocompleteAdapter(
+            collections=[
+                "prosnet-wikidata-place-index",
+                "prosnet-geonames-place-index",
+            ],
+            token=os.getenv("TYPESENSE_TOKEN", None),
+            server=os.getenv("TYPESENSE_SERVER", None),
+        ),
+        LobidAutocompleteAdapter(
+            params={
+                "filter": "type:PlaceOrGeographicName",
+                "format": "json:preferredName",
+            }
+        ),
+    ]
+
+
+class PersonExternalAutocomplete(ExternalAutocomplete):
+    adapters = [
+        TypeSenseAutocompleteAdapter(
+            collections="prosnet-wikidata-person-index",
+            token=os.getenv("TYPESENSE_TOKEN", None),
+            server=os.getenv("TYPESENSE_SERVER", None),
+        ),
+        LobidAutocompleteAdapter(
+            params={
+                "filter": "type:Person",
+                "format": "json:preferredName,professionOrOccupation",
+            }
+        ),
+    ]
