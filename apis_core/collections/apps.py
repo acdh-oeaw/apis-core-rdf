27f7e8d2300fe819508bd992293d5d84376dcbfc
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-08-12 13:26:29 +0200

    feat(collections): allow add all changes in a session to a collection
    This commit implements a way to add SkosCollections to the users
    session. If there are SkosCollections in the session and an instance
    of an apis_core.history model gets saved, that instance gets tagged
    with the SkosCollections.
    
    Closes: #1066

--- apis_core/collections/apps.py
+++ apis_core/collections/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class VocabsConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "apis_core.collections"
+
+    def ready(self):
+        from . import signals  # noqa: F401
