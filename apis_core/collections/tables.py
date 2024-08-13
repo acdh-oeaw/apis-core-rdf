fd250c7f52a3703b8d222481eef557bc1aaf029e
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-08-13 11:48:35 +0200

    feat(collections): introduce custom collections table
    The custom collections table contains a `target` column that lists, with
    a link, the instance the collection is pointing to.

---
+++ apis_core/collections/tables.py
@@ -0,0 +1,9 @@
+from django_tables2 import TemplateColumn
+from apis_core.generic.tables import GenericTable
+
+
+class SkosCollectionContentObjectTable(GenericTable):
+    target = TemplateColumn(
+        "<a href='{{ record.content_object.get_absolute_url }}'>{{ record.content_object }}</a>",
+        orderable=False,
+    )
