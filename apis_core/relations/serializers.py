9bb30c3ffd63a25f7f1cacb6fba653595047735e
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-08-30 09:44:23 +0200

    feat(relations): override the default serializer for relations
    This replaces the default serializer for relations. Instead of the full
    blown GenericHyperlinkedModelSerializer, we use the smaller
    SimpleObjectSerializer and add the `subj` and the `obj` also using the
    SimpleObjectSerializer.

---
+++ apis_core/relations/serializers.py
@@ -0,0 +1,21 @@
+from drf_spectacular.utils import extend_schema_field
+from rest_framework.serializers import SerializerMethodField
+from apis_core.generic.serializers import SimpleObjectSerializer, serializer_factory
+
+
+class RelationSerializer(SimpleObjectSerializer):
+    subj = SerializerMethodField()
+    obj = SerializerMethodField()
+
+    class Meta:
+        fields = SimpleObjectSerializer.Meta.fields + ["subj", "obj"]
+
+    @extend_schema_field(SimpleObjectSerializer())
+    def get_subj(self, obj):
+        serializer = serializer_factory(type(obj.subj), SimpleObjectSerializer)
+        return serializer(obj.subj, context={"request": self.context["request"]}).data
+
+    @extend_schema_field(SimpleObjectSerializer())
+    def get_obj(self, obj):
+        serializer = serializer_factory(type(obj.obj), SimpleObjectSerializer)
+        return serializer(obj.obj, context={"request": self.context["request"]}).data
