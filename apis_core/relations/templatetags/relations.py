3e398589f5c3ea1c7d8d776d9379df7965591449
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-08-30 08:30:50 +0200

    feat(relations): add a relations_verbose_name_listview_url templatetag

--- apis_core/relations/templatetags/relations.py
+++ apis_core/relations/templatetags/relations.py
@@ -70,3 +70,18 @@ def relations_list_table(context, relations, target=None):
             if relation_match_target(relation, target)
         ]
     return table_class(relations, request=context["request"])
+
+
+@register.simple_tag
+def relations_verbose_name_listview_url():
+    """
+    Return all relations verbose names together with their list uri, sorted in alphabetical order
+    USED BY:
+    * `apis_core/relations/templates/base.html` (to extend the default `base.html`)
+    """
+    relation_classes = [relation.model_class() for relation in relation_content_types()]
+    ret = {
+        relation._meta.verbose_name: relation.get_listview_url()
+        for relation in relation_classes
+    }
+    return sorted(ret.items())
