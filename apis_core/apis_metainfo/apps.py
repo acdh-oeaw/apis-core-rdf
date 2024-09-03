d7f6d042a89a3dc858b0e81a8158f8466d2cc8d5
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-03 16:33:20 +0200

    feat(apis_metainfo): print warning when APIS_BASE_URI is not set

--- apis_core/apis_metainfo/apps.py
+++ apis_core/apis_metainfo/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class MetainfoConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "apis_core.apis_metainfo"
+
+    def ready(self):
+        if getattr(settings, "APIS_BASE_URI", None) is None:
+            logger.warning(
+                "You should set the APIS_BASE_URI setting - we are using https://example.org as a fallback!"
+            )
