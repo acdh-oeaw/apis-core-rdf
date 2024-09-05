863afab505b97ae6ad37d326e7197553a4d22486
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-05 11:59:03 +0200

    fix(apis_metainfo): drop UriGetOrCreate view and related stuff
    The UriGetOrCreate view used the UriGetOrCreateForm, which did not what
    the name suggested: it did get_or_create an Uri, but if the created uri
    instance did not link to a root_object it rightaway deleted the it and
    raised a ValidationError. This basically boils down to a simple `get`.
    
    This also removes the templates used by the view, the mentioned form, and
    the whole urls module, which only pointed to the view.
    
    Closes: #1135

--- apis_core/apis_metainfo/forms.py
+++ apis_core/apis_metainfo/forms.py
@@ -1,10 +1,4 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-from django import forms
-from django.core.exceptions import ValidationError
 from apis_core.generic.forms import GenericModelForm
-
-from .models import Uri
 
 
 class UriForm(GenericModelForm):
@@ -13,22 +7,3 @@ class UriForm(GenericModelForm):
         self.helper.form_class = "form-horizontal"
         self.helper.label_class = "col-md-3"
         self.helper.field_class = "col-md-9"
-
-
-class UriGetOrCreateForm(forms.Form):
-    uri = forms.URLField()
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.add_input(
-            Submit("submit", "save"),
-        )
-
-    def clean_uri(self):
-        self.uriobj, _ = Uri.objects.get_or_create(uri=self.cleaned_data["uri"])
-        # This is a workaround until we decide to make the `root_object` ForeignKey
-        # a required field
-        if not self.uriobj.root_object:
-            self.uriobj.delete()
-            raise ValidationError("Could not create object from Uri")
