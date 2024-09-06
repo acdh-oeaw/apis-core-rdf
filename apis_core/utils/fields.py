d967f78a2931a0e09cb172e011485c45ef55081c
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-06 10:12:39 +0200

    chore(utils): sort imports using ruff

--- apis_core/utils/fields.py
+++ apis_core/utils/fields.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from apis_core.generic.forms.widgets import NewlineSeparatedListWidget
 
 
--- apis_core/utils/settings.py
+++ apis_core/utils/settings.py
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: MIT
 
 import logging
-import tomllib
 from pathlib import Path
+
+import tomllib
 from django.conf import settings
 from django.template.utils import get_app_template_dirs
 
