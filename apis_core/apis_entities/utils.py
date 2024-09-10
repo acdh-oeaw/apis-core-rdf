0be8476979ac8a55c0bc520619d97f771b48e2e3
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-10 10:56:32 +0200

    feat(apis_entities): add a util method to get all entity conten types

--- apis_core/apis_entities/utils.py
+++ apis_core/apis_entities/utils.py
@@ -1,7 +1,12 @@
 from django.apps import apps
+from django.contrib.contenttypes.models import ContentType
 
 from apis_core.apis_entities.models import AbstractEntity
 
 
 def get_entity_classes():
     return list(filter(lambda x: issubclass(x, AbstractEntity), apps.get_models()))
+
+
+def get_entity_content_types():
+    return [ContentType.objects.get_for_model(model) for model in get_entity_classes()]
