9192e97653735e8033efb9c1425bbde69c5bed2c
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-12 12:37:24 +0200

    fix(relations): set model of RelationSerializer
    This makes the swagger generation work again
    
    Closes: #1178

--- apis_core/relations/serializers.py
+++ apis_core/relations/serializers.py
@@ -2,6 +2,7 @@ from drf_spectacular.utils import extend_schema_field
 from rest_framework.serializers import SerializerMethodField
 
 from apis_core.generic.serializers import SimpleObjectSerializer, serializer_factory
+from apis_core.relations.models import Relation
 
 
 class RelationSerializer(SimpleObjectSerializer):
@@ -10,6 +11,7 @@ class RelationSerializer(SimpleObjectSerializer):
 
     class Meta:
         fields = SimpleObjectSerializer.Meta.fields + ["subj", "obj"]
+        model = Relation
 
     @extend_schema_field(SimpleObjectSerializer())
     def get_subj(self, obj):
