f9db2bce10791b4df1693f77d97596286b6b15e5
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-20 13:00:08 +0200

    feat(relations): replace bootstrap modal with html dialog
    HTML now supports a `dialog` element which can be used do implement a
    modal like behaviour. Using this `dialog` element is a more standardized
    approach than using bootstraps modal element.
    This commit replaces the use of the globally defined bootstrap modal in
    the apis base.html template and uses a dialog instead.

---
+++ apis_core/relations/static/js/relation_dialog.js
@@ -0,0 +1,20 @@
+document.body.addEventListener("reinit_select2", function(evt) {
+    form = document.getElementById(evt.detail.value);
+    form.querySelectorAll(".listselect2").forEach(element => {
+        $(element).select2({
+            ajax: {
+                url: $(element).data("autocomplete-light-url"),
+            },
+            dropdownParent: $("#relationdialog"),
+        });
+    });
+    $('.select2-selection').addClass("form-control");
+});
+document.body.addEventListener("dismissModal", function(evt) {
+    document.getElementById("relationdialog").close();
+});
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("relationdialog").addEventListener("mousedown", function(evt) {
+        evt.target == this && this.close();
+    });
+});
