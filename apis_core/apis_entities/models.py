18be45afa56a77f9e7a0cac56325593baa7d07c0
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-09-25 09:19:18 +0200

    refactor(apis_metainfo)!: drop unused attributes from Uri model

--- apis_core/apis_entities/models.py
+++ apis_core/apis_entities/models.py
@@ -160,4 +160,4 @@ def create_default_uri(sender, instance, created, raw, using, update_fields, **k
                     "apis_core:GetEntityGeneric", kwargs={"pk": instance.pk}
                 )
             uri = f"{base}{route}"
-            Uri.objects.create(uri=uri, domain="apis default", root_object=instance)
+            Uri.objects.create(uri=uri, root_object=instance)
