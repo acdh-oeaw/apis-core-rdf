57ff120c6f0f0dd94a864c013947fd086d8c17a1
KK <dev-kk@oeaw.ac.at>
2024-10-03 18:33:27 +0200

    refactor(core): move scroll button JS
    Move internal script from included partial
    template backtotopbtn.html to external core.js
    file.

--- apis_core/core/static/js/core.js
+++ apis_core/core/static/js/core.js
@@ -21,6 +21,36 @@ function configHtmx() {
   });
 }
 
+/* scroll-to-top button */
+function scrollButton() {
+
+  //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+}
+
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "interactive") {
     // equivalent to DOMContentLoaded; document has loaded, defered scripts
@@ -30,5 +60,6 @@ document.addEventListener("readystatechange", (event) => {
 
     configMultiSelect();
     configHtmx();
+    scrollButton();
   }
 });
