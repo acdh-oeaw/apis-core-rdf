8ce3f10c1ad563f5eace0e7def7f818b935b60c7
KK <dev-kk@oeaw.ac.at>
2024-10-03 18:22:36 +0200

    feat(core): add JS event listener
    Add event listener for readystatechange
    to core.js.

--- apis_core/core/static/js/core.js
+++ apis_core/core/static/js/core.js
@@ -5,3 +5,12 @@ $(document).ready(function() {
         enableFiltering: true
     });
 })
+
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "interactive") {
+    // equivalent to DOMContentLoaded; document has loaded, defered scripts
+    // have downloaded and executed, sub-resources may not be ready yet
+  } else if (event.target.readyState === "complete") {
+    // fired when fully loaded, incl. sub-resources and async scripts
+  }
+});
