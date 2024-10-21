1dcafd11172f6f7fdb8530df8a5ad0dd21995f99
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-10-21 11:37:29 +0200

    feat(relations): implement duplicate and merge signal receivers
    Closes: #1251

--- apis_core/relations/apps.py
+++ apis_core/relations/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RelationsNgConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "apis_core.relations"
+
+    def ready(self):
+        from . import signals  # noqa: F401
