32c8f4eeee2e4bca04d04020746c7872a796f20b
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-10-23 10:59:37 +0200

    feat(sample_project): automaticall add admin
    Closes: #1292

--- sample_project/entrypoint.sh
+++ sample_project/entrypoint.sh
@@ -3,4 +3,8 @@
 /app/manage.py migrate
 /app/manage.py setup || true
 /app/manage.py loaddata sample_project --settings=sample_project.settings_loaddata
+if [ -n "${DJANGO_SUPERUSER_PASSWORD+x}" ]; then
+  echo "Creating adchadmin user with password $DJANGO_SUPERUSER_PASSWORD"
+  /app/manage.py createsuperuser --email acdhadmin@discworld.acdh-ch-dev.oeaw.ac.at --user acdhadmin --noinput
+fi
 /app/manage.py runserver 0.0.0.0:5000
