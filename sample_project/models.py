041be63ccefe62e5e75ab1005115ee8f8b061e5f
Gytha Ogg <gythaoggscat@gmail.com>
2024-11-05 12:46:44 +0100

    feat(sample_project): annotated queryset for Place
    Place queryset is annotated with two fields called resident_id, and
    resident_name that picks one resident (person) living in that place
    using the LivesIn relationship.

--- sample_project/models.py
+++ sample_project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import OuterRef, QuerySet, Subquery
 
 from apis_core.apis_entities.abc import E21_Person, E53_Place, E74_Group
 from apis_core.apis_entities.models import AbstractEntity
@@ -18,8 +19,28 @@ class Person(VersionMixin, E21_Person, AbstractEntity):
     profession = models.ManyToManyField(Profession, blank=True)
 
 
+class PlaceQuerySet(QuerySet):
+    def with_a_resident(self):
+        return self.annotate(
+            # Subquery to get the name of a resident
+            resident_id=Subquery(
+                LivesIn.objects.filter(obj_object_id=OuterRef("id")).values(
+                    "subj_object_id"
+                )[:1]
+            ),
+            resident_name=Subquery(
+                Person.objects.filter(id=OuterRef("resident_id")).values("forename")[:1]
+            ),
+        )
+
+
+class PlaceManager(models.Manager):
+    def get_queryset(self):
+        return PlaceQuerySet(self.model, using=self._db).with_a_resident()
+
+
 class Place(VersionMixin, E53_Place, AbstractEntity):
-    pass
+    objects = PlaceManager()
 
 
 class Group(VersionMixin, E74_Group, AbstractEntity):
