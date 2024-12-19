e80e62eeefbc3200fa66f95ccff4ddaddaf6cb0f
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-19 10:51:23 +0100

    feat(utils): introduce a rdf_namespace_prefix settings helper

--- apis_core/utils/settings.py
+++ apis_core/utils/settings.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 import logging
+from urllib.parse import urlparse
 
 import tomllib
 from django.conf import settings
@@ -39,3 +40,10 @@ def internal_uris() -> list[str]:
 
 def apis_base_uri() -> str:
     return getattr(settings, "APIS_BASE_URI", "https://example.org")
+
+
+def rdf_namespace_prefix() -> str:
+    if hasattr(settings, "APIS_RDF_NAMESPACE_PREFIX"):
+        return settings.APIS_RDF_NAMESPACE_PREFIX
+    base_uri = urlparse(apis_base_uri())
+    return base_uri.hostname.split(".", 1)[0]
