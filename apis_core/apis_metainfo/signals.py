e64182c630086432764719517969b4db7061f430
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-01-07 12:44:16 +0100

    feat(apis_metainfo): add a signal to remove unused Uris
    When an object in Django is deleted, all the Uris pointing to that
    object should also be deleted

---
+++ apis_core/apis_metainfo/signals.py
@@ -0,0 +1,20 @@
+import logging
+
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from apis_core.apis_metainfo.models import Uri
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_delete)
+def remove_stale_uris(sender, instance, *args, **kwargs):
+    content_type = ContentType.objects.get_for_model(instance)
+    uris = Uri.objects.filter(content_type=content_type, object_id=instance.id)
+    for uri in uris:
+        logger.info(
+            "Deleting uri %s as a result of deleting %s", repr(uri), repr(instance)
+        )
+        uri.delete()
