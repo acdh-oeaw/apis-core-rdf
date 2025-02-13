c8f94c2c98314092c76f707b67f25d6b9c7aa59d
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-02-13 08:55:31 +0100

    feat(relations): replace custom select2 call
    Instead of using select2 directly and thus circumenventing the
    django-autocomplete-light function registry and doing all the select2
    configuration manually, we now simply call the
    autocomplete-light-function that is defined on the element. This is also
    whats done on page load (but not when inserted using htmx).

--- apis_core/relations/static/js/relation_dialog.js
+++ apis_core/relations/static/js/relation_dialog.js
@@ -1,28 +1,15 @@
-function tohtml(item) {
-    const span = document.createElement('span');
-    span.innerHTML = item.text;
-    return span;
-}
-
 function rel_reinit_select2() {
-    document.querySelectorAll(".listselect2, .modelselect2multiple, .modelselect2").forEach(element => {
-        $(element).select2({
-            ajax: {
-                url: $(element).data("autocomplete-light-url"),
-            },
-            dropdownParent: $(element.form),
-            templateResult: tohtml,
-            templateSelection: tohtml,
-        });
+    document.querySelectorAll("[data-autocomplete-light-function]").forEach(element => {
+        var dalFunction = $(element).attr('data-autocomplete-light-function');
+        if (yl.functions.hasOwnProperty(dalFunction) && typeof yl.functions[dalFunction] == 'function') {
+            yl.functions[dalFunction]($, element);
+        }
     });
     $('.select2-selection').addClass("form-control");
 }
 
 rel_reinit_select2();
 
-document.body.addEventListener("reinit_select2", function(evt) {
-    rel_reinit_select2();
-});
 document.body.addEventListener("dismissModal", function(evt) {
     document.getElementById("relationdialog").close();
 });
