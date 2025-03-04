e1a8a958e92daa9770b674eae84ef80692931174
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-03-04 10:08:10 +0100

    refactor(apis_entities): drop AbstractEntity customizations
    We used some overrides to not show fields from the AbstractEntity or
    TempEntityClass models. Most of those custom classes are dropped by now,
    so we can also drop those customizations.
    
    Closes: #1599

--- apis_core/apis_entities/filtersets.py
+++ apis_core/apis_entities/filtersets.py
@@ -5,30 +5,8 @@ from django.forms import DateInput
 from django.utils.encoding import force_str
 from simple_history.utils import get_history_manager_for_model
 
-from apis_core.generic.filtersets import GenericFilterSet, GenericFilterSetForm
+from apis_core.generic.filtersets import GenericFilterSet
 from apis_core.generic.helpers import default_search_fields, generate_search_filter
-
-ABSTRACT_ENTITY_COLUMNS_EXCLUDE = [
-    "rootobject_ptr",
-    "triple_set_from_subj",
-    "triple_set_from_obj",
-    "uri",
-]
-
-ABSTRACT_ENTITY_FILTERS_EXCLUDE = ABSTRACT_ENTITY_COLUMNS_EXCLUDE + [
-    "review",
-    "start_date",
-    "start_start_date",
-    "start_end_date",
-    "end_date",
-    "end_start_date",
-    "end_end_date",
-    "notes",
-    "text",
-    "published",
-    "status",
-    "references",
-]
 
 
 class ModelSearchFilter(django_filters.CharFilter):
@@ -62,14 +40,8 @@ def changed_since(queryset, name, value):
     return queryset.filter(pk__in=ids)
 
 
-class AbstractEntityFilterSetForm(GenericFilterSetForm):
-    columns_exclude = ABSTRACT_ENTITY_COLUMNS_EXCLUDE
-
-
 class AbstractEntityFilterSet(GenericFilterSet):
     class Meta(GenericFilterSet.Meta):
-        form = AbstractEntityFilterSetForm
-        exclude = ABSTRACT_ENTITY_FILTERS_EXCLUDE
         filter_overrides = {
             models.CharField: {
                 "filter_class": django_filters.CharFilter,
