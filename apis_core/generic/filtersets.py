6bd93fe2130f970ab07677fd7a6f8301a6974364
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-03-13 09:56:14 +0100

    refactor(generic): drop CollectionsFilter and IncludeExcludeField
    CollectionsFilter got replaced by CollectionsIncludeExcludeFilter and
    the IncludeExcludeField was only used by the CollectionsFilter and can
    therefore be dropped. We also drop the IncludeExcludeMultiWidget which
    was only used by the IncludeExcludeField.

--- apis_core/generic/filtersets.py
+++ apis_core/generic/filtersets.py
@@ -2,49 +2,13 @@ import logging
 
 import django_filters
 from django.apps import apps
-from django.contrib.contenttypes.models import ContentType
 from django_filters.filterset import FilterSet
 
-from apis_core.generic.forms.fields import IncludeExcludeField
 from apis_core.collections.filters import CollectionsIncludeExcludeFilter
 
 from .forms import GenericFilterSetForm
 
 logger = logging.getLogger(__name__)
-
-
-class CollectionsFilter(django_filters.filters.ModelMultipleChoiceFilter):
-    """
-    A simple filter for connections to collections. It provides an `include`/`exclude`
-    selector, which allows to define what the filter should do.
-    """
-
-    @property
-    def field(self):
-        return IncludeExcludeField(super().field, required=self.extra["required"])
-
-    def filter(self, queryset, value):
-        try:
-            value, include_exclude = value
-        except ValueError:
-            pass
-        if value:
-            content_type = ContentType.objects.get_for_model(queryset.model)
-            try:
-                skoscollectioncontentobject = apps.get_model(
-                    "collections.SkosCollectionContentObject"
-                )
-                scco = skoscollectioncontentobject.objects.filter(
-                    content_type=content_type, collection__in=value
-                ).values("object_id")
-                match include_exclude:
-                    case "include":
-                        return queryset.filter(id__in=scco)
-                    case "exclude":
-                        return queryset.exclude(id__in=scco)
-            except LookupError as e:
-                logger.debug("Not filtering for collections: %s", e)
-        return queryset
 
 
 class GenericFilterSet(FilterSet):
--- apis_core/generic/forms/fields.py
+++ apis_core/generic/forms/fields.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.forms import ChoiceField, ModelChoiceField, MultiValueField, MultiWidget
+from django.forms import ModelChoiceField, MultiValueField, MultiWidget
 from django.utils.translation import gettext as _
 
 from apis_core.utils.helpers import create_object_from_uri
@@ -16,33 +16,6 @@ class ModelImportChoiceField(ModelChoiceField):
                 params={"value": value, "exception": e},
             )
         return result or super().to_python(value)
-
-
-class IncludeExcludeMultiWidget(MultiWidget):
-    template_name = "widgets/includeexclude_multiwidget.html"
-    use_fieldset = False
-
-    def decompress(self, value):
-        return [value, value]
-
-
-class IncludeExcludeField(MultiValueField):
-    """
-    This is a custom MultiValueField that adds a ChoiceField that only provides two
-    choices, namely `exclude` and `include`. It can be used for django-filter filters
-    to specify which action should be done with the filter.
-    """
-
-    def __init__(self, field, *args, **kwargs):
-        fields = (
-            field,
-            ChoiceField(choices=[("include", "include"), ("exclude", "exclude")]),
-        )
-        kwargs["widget"] = IncludeExcludeMultiWidget(widgets=[f.widget for f in fields])
-        super().__init__(fields=fields, *args, **kwargs)
-
-    def compress(self, data_list):
-        return data_list
 
 
 class RowColumnMultiWidget(MultiWidget):
