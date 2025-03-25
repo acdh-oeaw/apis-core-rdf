0dbf14a074943ab2de73360d76f17dc648402f5d
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-03-25 09:26:00 +0100

    feat(relations): ship some default relations
    Closes: #1104

---
+++ apis_core/relations/abc.py
@@ -0,0 +1,69 @@
+from django.utils.translation import gettext_lazy as _
+
+from apis_core.apis_entities.abc import E21_Person, E53_Place, E74_Group
+from apis_core.relations.models import Relation
+
+
+class IsMemberOf(Relation):
+    subj_model = E21_Person
+    obj_model = E74_Group
+
+    @classmethod
+    def name(self) -> str:
+        return _("is member of")
+
+    @classmethod
+    def reverse_name(self) -> str:
+        return _("has as member")
+
+
+class LivesIn(Relation):
+    subj_model = E21_Person
+    obj_model = E53_Place
+
+    @classmethod
+    def name(self) -> str:
+        return _("lives in")
+
+    @classmethod
+    def reverse_name(self) -> str:
+        return _("has habitant")
+
+
+class IsLocatedIn(Relation):
+    subj_model = E53_Place
+    obj_model = E53_Place
+
+    @classmethod
+    def name(self) -> str:
+        return _("is located in")
+
+    @classmethod
+    def reverse_name(self) -> str:
+        return _("is location of")
+
+
+class BornIn(Relation):
+    subj_model = E21_Person
+    obj_model = E53_Place
+
+    @classmethod
+    def name(self) -> str:
+        return _("born in")
+
+    @classmethod
+    def reverse_name(self) -> str:
+        return _("is birth place of")
+
+
+class DiedIn(Relation):
+    subj_model = E21_Person
+    obj_model = E53_Place
+
+    @classmethod
+    def name(self) -> str:
+        return _("died in")
+
+    @classmethod
+    def reverse_name(self) -> str:
+        return _("is place of death of")
