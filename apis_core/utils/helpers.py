57407cd9f0a9a89100f4ee2a499eb8cee259cf0d
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-03-27 07:28:19 +0100

    feat(utils): introduce TypeCheckerMetaClass
    This metaclass checks if all the attributes adhere to their type
    annotations. Child classes have to adhere to their parents type
    annotations.

--- apis_core/utils/helpers.py
+++ apis_core/utils/helpers.py
@@ -1,4 +1,5 @@
 import difflib
+from collections import ChainMap
 
 from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
@@ -182,3 +183,22 @@ def flatten_if_single(value: list):
     if len(value) == 1:
         return value[0]
     return value
+
+
+class TypeCheckerMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        annotations = attrs.get("__annotations__", {})
+        for base in bases:
+            annotations.update(
+                ChainMap(
+                    *(
+                        c.__annotations__
+                        for c in base.__mro__
+                        if "__annotations__" in c.__dict__
+                    )
+                )
+            )
+        for key, val in annotations.items():
+            if key in attrs and type(attrs[key]) is not val:
+                raise ValueError(f"{name}.{key} should be {val}")
+        return super().__new__(cls, name, bases, attrs)
