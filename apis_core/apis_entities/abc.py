0d14ff40512e750019dac298b8ad147e51420fcd
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-03-27 07:43:01 +0100

    feat(apis_entities): add metadata & verbose names for abstract models

--- apis_core/apis_entities/abc.py
+++ apis_core/apis_entities/abc.py
@@ -14,14 +14,27 @@ from django.utils.translation import gettext_lazy as _
 
 
 class E21_Person(models.Model):
-    forename = models.CharField(blank=True, default="", max_length=4096)
-    surname = models.CharField(blank=True, default="", max_length=4096)
-    gender = models.CharField(blank=True, default="", max_length=4096)
-    date_of_birth = models.DateField(blank=True, null=True)
-    date_of_death = models.DateField(blank=True, null=True)
+    forename = models.CharField(
+        blank=True, default="", max_length=4096, verbose_name=_("forname")
+    )
+    surname = models.CharField(
+        blank=True, default="", max_length=4096, verbose_name=_("surname")
+    )
+    gender = models.CharField(
+        blank=True, default="", max_length=4096, verbose_name=_("gender")
+    )
+    date_of_birth = models.DateField(
+        blank=True, null=True, verbose_name=_("date of birth")
+    )
+    date_of_death = models.DateField(
+        blank=True, null=True, verbose_name=_("date of death")
+    )
 
     class Meta:
         abstract = True
+        verbose_name = _("person")
+        verbose_name_plural = _("persons")
+        ordering = ["surname", "forename"]
 
     def __str__(self):
         return f"{self.forename} {self.surname}"
@@ -40,18 +53,24 @@ class E53_Place(models.Model):
     listed on https://www.geonames.org/export/codes.html
     """
 
-    label = models.CharField(blank=True, default="", max_length=4096)
-    latitude = models.FloatField(blank=True, null=True)
-    longitude = models.FloatField(blank=True, null=True)
+    label = models.CharField(
+        blank=True, default="", max_length=4096, verbose_name=_("label")
+    )
+    latitude = models.FloatField(blank=True, null=True, verbose_name=_("latitude"))
+    longitude = models.FloatField(blank=True, null=True, verbose_name=_("longitude"))
     feature_code = models.CharField(
         blank=True,
         default="",
         max_length=16,
+        verbose_name=_("feature code"),
         help_text='<a href="https://www.geonames.org/export/codes.html">Geonames Feature Code List</a>',
     )
 
     class Meta:
         abstract = True
+        verbose_name = _("place")
+        verbose_name_plural = _("places")
+        ordering = ["label"]
 
     def __str__(self):
         return self.label
@@ -66,10 +85,15 @@ class E53_Place(models.Model):
 
 
 class E74_Group(models.Model):
-    label = models.CharField(blank=True, default="", max_length=4096)
+    label = models.CharField(
+        blank=True, default="", max_length=4096, verbose_name=_("label")
+    )
 
     class Meta:
         abstract = True
+        verbose_name = _("group")
+        verbose_name_plural = _("groups")
+        ordering = ["label"]
 
     def __str__(self):
         return self.label
@@ -84,7 +108,7 @@ class E74_Group(models.Model):
 
 class SimpleLabelModel(models.Model):
     label = models.CharField(
-        blank=True, default="", max_length=4096, verbose_name=_("Label")
+        blank=True, default="", max_length=4096, verbose_name=_("label")
     )
 
     class Meta:
