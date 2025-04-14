7f1fce63d176f9b93dbca15c458afb9100acdc23
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-14 12:05:02 +0200

    feat(apis_entities): add a helper method to list geonames featurecodes
    Closes: #1670

--- apis_core/apis_entities/utils.py
+++ apis_core/apis_entities/utils.py
@@ -1,3 +1,6 @@
+import functools
+from pathlib import Path
+
 from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
 
@@ -10,3 +13,21 @@ def get_entity_classes():
 
 def get_entity_content_types():
     return [ContentType.objects.get_for_model(model) for model in get_entity_classes()]
+
+
+@functools.lru_cache
+def get_feature_codes():
+    try:
+        featurecode_list = [(None, "---")]
+        self = Path(__file__)
+        featurecodefile = self.parent / "featureCodes_en.txt"
+        featurecodes = featurecodefile.read_text()
+        featurecodes = [
+            line.split("\t")
+            for line in featurecodes.split("\n")
+            if len(line.split("\t")) == 3
+        ]
+        featurecode_list.extend([(el[0], el[0] + ": " + el[1]) for el in featurecodes])
+    except Exception:
+        featurecode_list = [(None, "None: could not read list of feature codes")]
+    return featurecode_list
