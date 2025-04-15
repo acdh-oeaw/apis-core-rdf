4ef0e09de53ebd4fc2cc8dd8523b12b53892cebe
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-15 09:16:33 +0200

    refactor(history): drop unused templatetag

--- apis_core/history/templatetags/history.py
+++ apis_core/history/templatetags/history.py
@@ -1,16 +1,9 @@
 from django import template
 from django.utils.safestring import mark_safe
 
-from apis_core.history.serializers import HistoryLogSerializer
 from apis_core.utils.helpers import get_html_diff
 
 register = template.Library()
-
-
-@register.filter
-def get_history_data(obj):
-    data = HistoryLogSerializer(obj.get_history_data(), many=True).data
-    return data
 
 
 @register.filter
