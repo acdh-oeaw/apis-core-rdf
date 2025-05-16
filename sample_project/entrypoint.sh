c30d185bb4a2ca29b0d909bdca25445816d8d52b
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-16 10:59:44 +0200

    feat(sample_project): compile translations on startup

--- sample_project/entrypoint.sh
+++ sample_project/entrypoint.sh
@@ -3,6 +3,7 @@
 /app/manage.py migrate
 /app/manage.py setup || true
 /app/manage.py loaddata sample_project --settings=sample_project.settings_loaddata
+/app/manage.py compilemessages
 if [ -n "${DJANGO_SUPERUSER_PASSWORD+x}" ]; then
   echo "Creating adchadmin user with password $DJANGO_SUPERUSER_PASSWORD"
   /app/manage.py createsuperuser --email acdhadmin@discworld.acdh-ch-dev.oeaw.ac.at --user acdhadmin --noinput
