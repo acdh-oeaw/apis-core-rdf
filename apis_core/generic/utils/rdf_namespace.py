e729ee0a504ab61fec67f27205ba88251f620199
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-19 12:52:01 +0200

    fix(generic): fix typo in rdf APPELLATION namespace

--- apis_core/generic/utils/rdf_namespace.py
+++ apis_core/generic/utils/rdf_namespace.py
@@ -4,5 +4,5 @@ from apis_core.utils.settings import apis_base_uri
 
 CRM = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 
-APPELLATION = Namespace(apis_base_uri() + "/appelation/")
+APPELLATION = Namespace(apis_base_uri() + "/appellation/")
 ATTRIBUTES = Namespace(apis_base_uri() + "/attributes/")
