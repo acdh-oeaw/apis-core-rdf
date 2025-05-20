5097096c335420f3ff6f2bccee3f6f7f83f4b33c
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-20 12:19:10 +0200

    feat(history): introduce view to reset object to a history version
    Closes: #283

--- apis_core/history/views.py
+++ apis_core/history/views.py
@@ -1,9 +1,12 @@
-from django.views.generic.detail import DetailView
+from django.forms import Form
+from django.views.generic.detail import BaseDetailView, DetailView
+from django.views.generic.edit import FormView
 from django_tables2 import SingleTableMixin
 from django_tables2.tables import table_factory
 from simple_history.utils import get_history_model_for_model
 
 from apis_core.generic.helpers import first_member_match, module_paths
+from apis_core.generic.views import GenericModelMixin
 
 from .tables import HistoryGenericTable
 
@@ -21,3 +24,15 @@ class HistoryView(SingleTableMixin, DetailView):
         table_modules = module_paths(self.model, path="tables", suffix="HistoryTable")
         table_class = first_member_match(table_modules, HistoryGenericTable)
         return table_factory(self.model, table_class)
+
+
+class HistoryReset(GenericModelMixin, BaseDetailView, FormView):
+    form_class = Form
+    template_name = "history/reset.html"
+
+    def form_valid(self, form):
+        self.get_object().instance.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.get_object().instance.get_history_url()
