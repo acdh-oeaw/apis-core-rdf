efeaea57ce5a71bc060ca398474eb5440d6e0410
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-22 14:24:16 +0200

    refactor(history): drop unused create_new_version view
    ... and associated route and model method

--- apis_core/history/views.py
+++ apis_core/history/views.py
@@ -1,7 +1,3 @@
-from django.contrib.contenttypes.models import ContentType
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.utils import timezone
 from django.views.generic.detail import DetailView
 from django_tables2 import SingleTableMixin
 from django_tables2.tables import table_factory
@@ -25,22 +21,3 @@ class HistoryView(SingleTableMixin, DetailView):
         table_modules = module_paths(self.model, path="tables", suffix="HistoryTable")
         table_class = first_member_match(table_modules, HistoryGenericTable)
         return table_factory(self.model, table_class)
-
-
-def create_new_version(request, contenttype, pk):
-    """Gets the version of the history instance and creates a new version."""
-    model = contenttype.model_class()
-    instance = model.objects.get(id=pk)
-    history_latest = instance.history.latest()
-    history_latest.history_id = None
-    history_latest.history_date = timezone.now()
-    history_latest.save()
-    return redirect(
-        reverse(
-            "apis_core:generic:detail",
-            args=[
-                ContentType.objects.get_for_model(history_latest.__class__),
-                history_latest.history_id,
-            ],
-        )
-    )
