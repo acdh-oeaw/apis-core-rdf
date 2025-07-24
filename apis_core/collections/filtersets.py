525bb87d5f1fc282e9a2d0cf0e2b14de70eabf9b
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-07-24 07:31:48 +0200

    feat(collections): add custom filterset for SkosCollectionContentObject
    This filterset removes the `collections` filter from the filterset,
    because SkosCollectionContentObjects are rarely put into collections.
    It also limits the number of listed content_types to the ones that are
    actually used in SkosCollectionContentObjects.

---
+++ apis_core/collections/filtersets.py
@@ -0,0 +1,14 @@
+from django.contrib.contenttypes.models import ContentType
+
+from apis_core.collections.models import SkosCollectionContentObject
+from apis_core.generic.filtersets import GenericFilterSet
+
+
+class SkosCollectionContentObjectFilterSet(GenericFilterSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        del self.filters["collections"]
+        content_types = SkosCollectionContentObject.objects.all().values("content_type")
+        self.filters["content_type"].queryset = ContentType.objects.filter(
+            pk__in=content_types
+        )
