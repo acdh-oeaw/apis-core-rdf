0bc979ec659dae52f95f1c019699753687039ede
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-07-30 12:03:10 +0200

    fix(collections): check for filter existence before removing
    The collections filter only gets added if there are some collections. We
    add a check if it has been added before we remove it in this inheriting
    filterset, otherwise we get a KeyError.
    
    Closes: #1978

--- apis_core/collections/filtersets.py
+++ apis_core/collections/filtersets.py
@@ -7,7 +7,8 @@ from apis_core.generic.filtersets import GenericFilterSet
 class SkosCollectionContentObjectFilterSet(GenericFilterSet):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        del self.filters["collections"]
+        if "collections" in self.filters:
+            del self.filters["collections"]
         content_types = SkosCollectionContentObject.objects.all().values("content_type")
         self.filters["content_type"].queryset = ContentType.objects.filter(
             pk__in=content_types
