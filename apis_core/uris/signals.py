fa656d05de887938ae62e4bb503c17f875e892e7
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-08-25 08:25:09 +0200

    feat(uris): replace hardcoded type check with method call
    Instead of hardcoding a check that tests if the model instance is a
    child class of AbstractEntity, we now check if the model instance has
    a method that returns a default uri. This makes the signal handler more
    generic- in theory it could now also be possible for other models than
    AbstractEntity to get default URIs. It is also possible for projects to
    override the `get_default_uri` method, to customize the default uris.

--- apis_core/uris/signals.py
+++ apis_core/uris/signals.py
@@ -4,12 +4,9 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from django.urls import NoReverseMatch, reverse
 
-from apis_core.apis_entities.models import AbstractEntity
 from apis_core.generic.signals import post_merge_with
 from apis_core.uris.models import Uri
-from apis_core.utils.settings import apis_base_uri
 
 logger = logging.getLogger(__name__)
 
@@ -33,22 +30,17 @@ def create_default_uri(sender, instance, created, raw, using, update_fields, **k
         return
     create_default_uri = getattr(settings, "CREATE_DEFAULT_URI", True)
     skip_default_uri = getattr(instance, "skip_default_uri", False)
-    if create_default_uri and not skip_default_uri:
-        if isinstance(instance, AbstractEntity) and created:
-            base = apis_base_uri().strip("/")
-            try:
-                route = reverse("GetEntityGenericRoot", kwargs={"pk": instance.pk})
-            except NoReverseMatch:
-                route = reverse(
-                    "apis_core:GetEntityGeneric", kwargs={"pk": instance.pk}
-                )
-            uri = f"{base}{route}"
-            content_type = ContentType.objects.get_for_model(instance)
-            Uri.objects.create(
-                uri=uri,
-                content_type=content_type,
-                object_id=instance.id,
-            )
+    try:
+        uri = instance.get_default_uri()
+    except AttributeError:
+        uri = False
+    if create_default_uri and not skip_default_uri and uri and created:
+        content_type = ContentType.objects.get_for_model(instance)
+        Uri.objects.create(
+            uri=uri,
+            content_type=content_type,
+            object_id=instance.id,
+        )
 
 
 @receiver(post_merge_with)
