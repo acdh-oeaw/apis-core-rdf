717d49d5673c09029012a9fbd251eefc9fcab2d1
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-09-05 08:58:53 +0200

    feat(apis_entities): allow to override field names in placelookup field

--- apis_core/apis_entities/fields.py
+++ apis_core/apis_entities/fields.py
@@ -79,3 +79,23 @@ class ModelSelect2Multiple(Select2WidgetMixin, DALModelSelect2Multiple):
 
 class PlaceLookupField(forms.CharField):
     widget = PlaceLookup
+
+    def __init__(
+        self,
+        *args,
+        label_id="id_label",
+        longitude_id="id_longitude",
+        latitude_id="id_latitude",
+        **kwargs,
+    ):
+        self.label_id = label_id
+        self.longitude_id = longitude_id
+        self.latitude_id = latitude_id
+        super().__init__(*args, **kwargs)
+
+    def widget_attrs(self, widget):
+        attrs = super().widget_attrs(widget)
+        attrs["data-label-id"] = self.label_id
+        attrs["data-longitude-id"] = self.longitude_id
+        attrs["data-latitude-id"] = self.latitude_id
+        return attrs
