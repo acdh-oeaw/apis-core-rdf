daa81a119cf2814a8b8891a170aedd375a1e9dd5
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-09-12 11:23:47 +0200

    feat(apis_entities): override feature_code field in E53_PlaceForm
    We override the field to set the choices. We don't do this in the model
    because we don't want to create a migration everytime the featurecode
    list changes.

--- apis_core/apis_entities/forms.py
+++ apis_core/apis_entities/forms.py
@@ -1,6 +1,8 @@
+from django.forms.fields import ChoiceField
 from django.utils.translation import gettext_lazy as _
 
 from apis_core.apis_entities.fields import PlaceLookupField
+from apis_core.apis_entities.utils import get_feature_codes
 from apis_core.generic.forms import GenericModelForm
 
 
@@ -10,4 +12,6 @@ class E53_PlaceForm(GenericModelForm):
         label=_("Search places"),
         help_text=_("Click on a result to fill the form"),
     )
+    feature_code = ChoiceField(choices=get_feature_codes, required=False)
+
     field_order = ["place"]
