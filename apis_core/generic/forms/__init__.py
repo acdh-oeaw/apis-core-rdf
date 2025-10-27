47e6a8d63e995572aa23a4b9f7fa385db3644c03
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-10-27 09:57:51 +0100

    fix(generic): use correct method in GenericSelectMergeOrEnrichForm

--- apis_core/generic/forms/__init__.py
+++ apis_core/generic/forms/__init__.py
@@ -148,7 +148,7 @@ class GenericSelectMergeOrEnrichForm(forms.Form):
 
     def clean_uri(self):
         uri = self.cleaned_data["uri"]
-        if uri.isdigit() or self.content_type.model_class().valid_uri(uri):
+        if uri.isdigit() or self.content_type.model_class().valid_import_url(uri):
             return uri
         raise ValueError(f"{uri} is neither an ID nor something we can import")
 
