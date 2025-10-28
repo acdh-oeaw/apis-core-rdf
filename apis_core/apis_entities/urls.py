ecde169db05c6238ce0c6e144193fae013bc912b
Saranya Balasubramanian <saranya.balasubramanian@oeaw.ac.at>
2025-10-28 14:49:09 +0100

    chore(apis_entities): remove commented obsolete Reversion import
    The TODO and the commented out import are no longer needed.

--- apis_core/apis_entities/urls.py
+++ apis_core/apis_entities/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
 from apis_core.apis_entities.api_views import GetEntityGeneric, ListEntityGeneric
-
-# from .views import ReversionCompareView TODO: add again when import is fixed
 from apis_core.apis_entities.views import E53_PlaceMap, EntitiesAutocomplete
 
 api_routes = [
