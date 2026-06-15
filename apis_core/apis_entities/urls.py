f192954f6cc2dc2f833e82262553a3f310baa659
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-06-15 07:48:54 +0200

    refactor(entities)!: move E53_PlaceMap to `entities.views`
    BREAKING-CHANGE: We move the `apis_core.apis_entities.E53_PlaceMap` view
    to `entities` together with its url route and the `e53_place_map.html`
    template and the `E53_Place_map.js`

--- apis_core/apis_entities/urls.py
+++ apis_core/apis_entities/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from apis_core.apis_entities.api_views import GetEntityGeneric, ListEntityGeneric
-from apis_core.apis_entities.views import E53_PlaceMap, EntitiesAutocomplete
+from apis_core.apis_entities.views import EntitiesAutocomplete
 
 api_routes = [
     path("entities/", ListEntityGeneric.as_view()),
@@ -17,5 +17,4 @@ app_name = "apis_entities"
 
 urlpatterns = [
     path("autocomplete/", EntitiesAutocomplete.as_view(), name="autocomplete"),
-    path("<contenttype:contenttype>/map", E53_PlaceMap.as_view(), name="e53_place_map"),
 ]
--- apis_core/entities/urls.py
+++ apis_core/entities/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apis_core.entities.views import CanonicalEntity
+from apis_core.entities.views import CanonicalEntity, E53_PlaceMap
 
 urlpatterns = [
-    path("entity/<int:pk>", CanonicalEntity.as_view(), name="canonical-entity")
+    path("entity/<int:pk>", CanonicalEntity.as_view(), name="canonical-entity"),
+    path("<contenttype:contenttype>/map", E53_PlaceMap.as_view(), name="e53_place_map"),
 ]
